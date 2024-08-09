d2d97ca0ea670e26d657494f1b7d798eed6b4389
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-08-09 18:33:56 -0300

    Separated View from Controller.

--- Modulite/Screens/Home/Controller/HomeViewController.swift
+++ Modulite/Screens/Home/Controller/HomeViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    let homeView = HomeView()
+    
+    override func loadView() {
+        self.view = homeView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
---
+++ Modulite/Screens/Home/View/HomeView.swift
@@ -0,0 +1,30 @@
+//
+//  HomeView.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 09/08/24.
+//
+
+import UIKit
+
+class HomeView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubviews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubviews() {
+        
+    }
+    
+    private func setupConstraints() {
+        
+    }
+}
+
