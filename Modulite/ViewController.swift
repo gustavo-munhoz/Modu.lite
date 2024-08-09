910456323cf5fc2845a36e148f562b908bc73ce3
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-08-09 17:43:17 -0300

    Initial Commit

---
+++ Modulite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 09/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ModuliteUITests/ModuliteUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ModuliteUITestsLaunchTests.swift
+//  ModuliteUITests
+//
+//  Created by Gustavo Munhoz Correa on 09/08/24.
+//
+
+import XCTest
+
+final class ModuliteUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
