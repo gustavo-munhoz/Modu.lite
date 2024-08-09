44a82089a5f36177a56ce06cead74a40f4c92be6
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-08-09 18:24:34 -0300

    Implemented coordinator that handles `HomeViewController` presenting.

--- Modulite/Coordinators/HomeCoordinator.swift
+++ Modulite/Coordinators/HomeCoordinator.swift
@@ -5,4 +5,18 @@
 //  Created by Gustavo Munhoz Correa on 09/08/24.
 //
 
-import Foundation
+import UIKit
+
+class HomeCoordinator: Coordinator {
+    var children: [Coordinator] = []
+    let router: Router
+    
+    init(router: Router) {
+        self.router = router
+    }
+    
+    func present(animated: Bool, onDismiss: (() -> Void)?) {
+        let vc = HomeViewController()
+        router.present(vc, animated: animated, onDismiss: onDismiss)
+    }
+}
