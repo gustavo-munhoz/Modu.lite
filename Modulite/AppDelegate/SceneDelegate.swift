c40baea735c3a0232b6c00d9a385af7b1bb695d2
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-08-09 18:27:56 -0300

    Implemented both `HomeCoordinator` and `SceneDelegateRouter`.

--- Modulite/AppDelegate/SceneDelegate.swift
+++ Modulite/AppDelegate/SceneDelegate.swift
@@ -10,13 +10,18 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    lazy var router = SceneDelegateRouter(window: window!)
+    lazy var coordinator = HomeCoordinator(router: router)
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        coordinator.present(animated: true, onDismiss: nil)
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
