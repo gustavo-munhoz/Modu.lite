a52a52e0c2885d2199721cb0e0815bad25600a82
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-08-10 13:09:17 -0300

    Implemented basic version of localization enum. Works by separating key from values for each enum case, and the name must be associated with the key added in `Localizable.xcstrings`.

--- Modulite/Localization/String+LocalizedKey.swift
+++ Modulite/Localization/String+LocalizedKey.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+extension String {
+    
+    enum LocalizedKey {
+        case example(value: Int)
+        case example2
+        
+        fileprivate var key: String {
+            if let nameRemovingValues = Mirror(reflecting: self).children.first?.label {
+                return nameRemovingValues
+            }
+            
+            return String(describing: self)
+        }
+        
+        fileprivate var values: [CVarArg] {
+            switch self {
+            
+            default:
+                return []
+            }
+        }
+    }
+    
+    static func localized(for key: LocalizedKey) -> String {
+        String(format: NSLocalizedString(key.key, comment: ""), arguments: key.values)
+    }
+}
