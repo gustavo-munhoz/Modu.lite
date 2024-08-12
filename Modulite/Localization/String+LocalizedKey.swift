f99d3ca41dc98ef41df41fd263524e4261014947
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-08-11 23:19:48 -0300

    Implemented a more powerful version of enum which uses reflection to get values, so we don't have to write each one by hand. This may cause an impact in performance, but it makes localizing strings much easier.

--- Modulite/Localization/String+LocalizedKey.swift
+++ Modulite/Localization/String+LocalizedKey.swift
@@ -9,10 +9,13 @@ import Foundation
 
 extension String {
     
+    /// Represents keys for localized strings in the app, allowing for dynamic localization with associated values.
     enum LocalizedKey {
-        case example(value: Int)
-        case example2
+        // MARK: - Computed Properties
         
+        /// Computes the key for localization by extracting the case name from the enum instance.
+        /// Uses reflection to find the label of the first child of the enum case, which represents the case name without associated values.
+        /// Falls back to the default description if no label is found.
         fileprivate var key: String {
             if let nameRemovingValues = Mirror(reflecting: self).children.first?.label {
                 return nameRemovingValues
@@ -21,15 +24,24 @@ extension String {
             return String(describing: self)
         }
         
+        /// Computes an array of `CVarArg` suitable for string formatting, reflecting the associated values of the enum case.
+        /// Uses reflection to access and cast the associated values to `CVarArg` for use in formatted strings.
         fileprivate var values: [CVarArg] {
-            switch self {
+            guard let associatedValue = Mirror(reflecting: self).children.first?.value else { return [] }
             
-            default:
-                return []
-            }
+            return Mirror(reflecting: associatedValue).children.compactMap { $0.value as? CVarArg }
         }
+
+        
+        // MARK: - Localized Keys
+        case <#code#>
     }
     
+    /// Returns a localized string using the key and associated values defined by the `LocalizedKey` enum.
+    /// Utilizes the `NSLocalizedString` function to fetch the appropriate translation for the key,
+    /// and formats it with any associated values using `String(format:arguments:)`.
+    /// - Parameter key: The `LocalizedKey` enum case representing the localization key and its associated values.
+    /// - Returns: A formatted, localized string.
     static func localized(for key: LocalizedKey) -> String {
         String(format: NSLocalizedString(key.key, comment: ""), arguments: key.values)
     }
