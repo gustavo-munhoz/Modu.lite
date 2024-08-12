c98107e49e18f1921272f551a44c5d1beb531395
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-08-12 00:33:32 -0300

    Implemented tests for localized strings.

--- ModuliteTests/Localization/LocalizationTests.swift
+++ ModuliteTests/Localization/LocalizationTests.swift
@@ -14,33 +14,44 @@ class LocalizationTests: XCTestCase {
     func testLocalizedKeyExtraction() {
         let testIntKey = String.LocalizedKey.testInteger(value: 123)
         XCTAssertEqual(testIntKey.key, "testInteger", "The key should correctly extract 'testInteger'.")
-
+        
+        let testStringKey = String.LocalizedKey.testString(text: "Hello")
+        XCTAssertEqual(testStringKey.key, "testString", "The key should correctly extract 'testString'.")
+        
+        let testArrayKey = String.LocalizedKey.testArray(elements: ["Hello", "World"])
+        XCTAssertEqual(testArrayKey.key, "testArray", "The key should correctly extract 'testArray'.")
+        
         let testNoValueKey = String.LocalizedKey.testNoValue
         XCTAssertEqual(testNoValueKey.key, "testNoValue", "The key should correctly extract 'testNoValue'.")
     }
-    
     
     func testLocalizedValuesExtraction() {
         let testIntKey = String.LocalizedKey.testInteger(value: 123)
         XCTAssertEqual(testIntKey.values as? [Int], [123], "The values should correctly extract [123].")
         
-//        let testStringKey = String.LocalizedKey.testString(text: "Hello, world!")
-//        XCTAssertEqual(testStringKey.values as? [String], "The values should correctly extract [Hello, world!].")
+        let testStringKey = String.LocalizedKey.testString(text: "Hello, world!")
+        XCTAssertEqual(testStringKey.values as? [String], ["Hello, world!"], "The values should correctly extract ['Hello, world!'].")
         
         let testArrayKey = String.LocalizedKey.testArray(elements: ["Hello", "World"])
         XCTAssertEqual(testArrayKey.values as? [String], ["Hello", "World"], "The values should correctly extract ['Hello', 'World']")
-
+        
         let testNoValueKey = String.LocalizedKey.testNoValue
-        XCTAssertTrue(testNoValueKey.values.isEmpty, "There should be no values associated with 'example2'.")
+        XCTAssertTrue(testNoValueKey.values.isEmpty, "There should be no values associated with 'testNoValue'.")
     }
     
     
     func testLocalizedMethod() {
 
-        let result1 = String.localized(for: .testInteger(value: 123))
-        XCTAssertEqual(result1, "Value: 123", "The localized string should correctly format.")
+        let intResult = String.localized(for: .testInteger(value: 123))
+        XCTAssertEqual(intResult, "Value: 123", "The localized string should correctly format.")
 
-        let result2 = String.localized(for: .testNoValue)
-        XCTAssertEqual(result2, "No values here.", "The localized string should match the key with no values.")
+        let strResult = String.localized(for: .testString(text: "Test string"))
+        XCTAssertEqual(strResult, "String: Test string", "The localized string should correctly format.")
+        
+        let arrayResult = String.localized(for: .testArray(elements: ["String 1", "String 2"]))
+        XCTAssertEqual(arrayResult, "Strings: String 1, String 2", "The localized string should correctly format.")
+        
+        let noneResult = String.localized(for: .testNoValue)
+        XCTAssertEqual(noneResult, "No values here.", "The localized string should match the key with no values.")
     }
 }
