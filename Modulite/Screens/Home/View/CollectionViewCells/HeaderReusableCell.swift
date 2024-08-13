45ce4dd2b5a44b479e9cda709f45d680da66ff7a
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-08-13 11:03:46 -0300

    Implemented first version of Header Cell, will make necessary adjustments later.

--- Modulite/Screens/Home/View/CollectionViewCells/HeaderReusableCell.swift
+++ Modulite/Screens/Home/View/CollectionViewCells/HeaderReusableCell.swift
@@ -5,4 +5,30 @@
 //  Created by Gustavo Munhoz Correa on 13/08/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class HeaderReusableCell: UICollectionViewCell {
+    static let reuseId = "HeaderReusableCell"
+    
+    private(set) lazy var titleLabel: UILabel = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        let fd = UIFontDescriptor.preferredFontDescriptor(withTextStyle: .title2)
+        let customFd = fd.addingAttributes([.traits: [
+            UIFontDescriptor.TraitKey.weight: UIFont.Weight.bold
+        ]])
+        view.font = UIFont(descriptor: customFd, size: 0)
+        view.textColor = .textPrimary
+        
+        return view
+    }()
+    
+    private(set) lazy var actionButton: UIButton = {
+        let view = UIButton()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+}
