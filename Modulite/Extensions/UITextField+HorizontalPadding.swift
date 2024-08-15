aaff2903d82186cc8c3c196b653fe88203c01a69
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-08-15 17:45:19 -0300

    Created extension to use `UITextField`s with horizontal padding.

---
+++ Modulite/Extensions/UITextField+HorizontalPadding.swift
@@ -0,0 +1,21 @@
+//
+//  UITextField+HorizontalPadding.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 15/08/24.
+//
+
+import UIKit
+
+extension UITextField {
+    func setLeftPaddingPoints(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    func setRightPaddingPoints(_ amount:CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
