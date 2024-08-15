90fa4c49c37d6be6a728abd574927e11ba91e640
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-08-15 17:45:51 -0300

    Created extensions to simplify font customizations through font descriptors.

---
+++ Modulite/Extensions/UIFont+TraitedTextStyle.swift
@@ -0,0 +1,37 @@
+//
+//  UIFont+TraitedTextStyle.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 15/08/24.
+//
+
+import UIKit
+
+extension UIFont {
+    convenience init(
+        textStyle: UIFont.TextStyle,
+        weight: UIFont.Weight
+    ) {
+        self.init(
+            descriptor:
+                .preferredFontDescriptor(withTextStyle: textStyle)
+                .addingAttributes([.traits: [
+                    UIFontDescriptor.TraitKey.weight: weight,
+                ]]),
+            size: 0
+        )
+    }
+    
+    convenience init?(
+        textStyle: UIFont.TextStyle,
+        symbolicTraits: UIFontDescriptor.SymbolicTraits
+    ) {
+        let baseDescriptor = UIFontDescriptor.preferredFontDescriptor(withTextStyle: textStyle)
+        
+        guard let descriptor = baseDescriptor.withSymbolicTraits(symbolicTraits) else {
+            return nil
+        }
+        
+        self.init(descriptor: descriptor, size: 0)
+    }
+}
