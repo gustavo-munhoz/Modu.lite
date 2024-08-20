20037b582303a270c123edd08d62c71cbefd2691
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-08-20 11:56:48 -0300

    Created basic version of `WidgetEditorViewController`.

--- Modulite/Screens/WidgetConfiguration/Controller/WidgetEditorViewController.swift
+++ Modulite/Screens/WidgetConfiguration/Controller/WidgetEditorViewController.swift
@@ -9,10 +9,21 @@ import UIKit
 
 class WidgetEditorViewController: UIViewController {
     
-    private var editorView = WidgetEditorView()
+    private let editorView = WidgetEditorView()
+    private let viewModel = WidgetEditorViewModel()
     
     override func loadView() {
         view = editorView
     }
     
+}
+
+extension WidgetEditorViewController {
+    class func instantiate(widgetId: UUID, delegate: HomeNavigationFlowDelegate) -> WidgetEditorViewController {
+        let vc = WidgetEditorViewController()
+        vc.viewModel.setWidgetId(to: widgetId)
+        vc.viewModel.setDelegate(to: delegate)
+        
+        return vc
+    }
 }
