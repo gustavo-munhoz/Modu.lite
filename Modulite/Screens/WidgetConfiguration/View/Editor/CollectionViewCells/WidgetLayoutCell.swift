a1000caeecf1f1e0273a068407a4598051f71b70
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-08-20 11:54:43 -0300

    Created basic version of layout cell, just to visualize in collection view.

--- Modulite/Screens/WidgetConfiguration/View/Editor/CollectionViewCells/WidgetLayoutCell.swift
+++ Modulite/Screens/WidgetConfiguration/View/Editor/CollectionViewCells/WidgetLayoutCell.swift
@@ -5,4 +5,25 @@
 //  Created by Gustavo Munhoz Correa on 20/08/24.
 //
 
-import Foundation
+import UIKit
+
+class WidgetLayoutCell: UICollectionViewCell {
+    static let reuseId = "WidgetLayoutCell"
+    
+    private(set) lazy var image: UIImageView = {
+        let view = UIImageView(image: UIImage(systemName: "house.fill"))
+        
+        return view
+    }()
+    
+    private func addSubviews() {
+        addSubview(image)
+    }
+    
+    private func setupConstraints() {
+        image.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
