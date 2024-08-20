9bf9adf48d57456eea31a3bcf6ae7680cf2e5dd5
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-08-20 11:46:53 -0300

    Renamed colors and created new essential files.

---
+++ Modulite/Screens/WidgetConfiguration/View/Editor/CollectionViewCells/WidgetLayoutCell.swift
@@ -0,0 +1,8 @@
+//
+//  WidgetLayoutCell.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 20/08/24.
+//
+
+import Foundation
---
+++ Modulite/Screens/WidgetConfiguration/View/Editor/EditorSectionHeader.swift
@@ -0,0 +1,8 @@
+//
+//  EditorSectionHeader.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 20/08/24.
+//
+
+import Foundation
