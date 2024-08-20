def952eda49669f3a0334ea03c5c6d3615e59061
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-08-20 15:15:29 -0300

    Implemented `UICollectionViewCompositionalLayout` for `widgetLayoutCollectionView`, as well as methods for setting delegate and datasource.

--- Modulite/Screens/WidgetConfiguration/View/Editor/CollectionViewCells/WidgetLayoutCell.swift
+++ Modulite/Screens/WidgetConfiguration/View/Editor/CollectionViewCells/WidgetLayoutCell.swift
@@ -16,6 +16,16 @@ class WidgetLayoutCell: UICollectionViewCell {
         return view
     }()
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubviews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     private func addSubviews() {
         addSubview(image)
     }
@@ -26,4 +36,3 @@ class WidgetLayoutCell: UICollectionViewCell {
         }
     }
 }
-
