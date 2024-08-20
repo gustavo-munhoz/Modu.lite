675f4a130811357fda5ac498d068fe1b4ecbe698
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-08-20 11:57:59 -0300

    Created basic version of `WidgetEditorViewModel`.

--- Modulite/Screens/WidgetConfiguration/ViewModel/WidgetEditorViewModel.swift
+++ Modulite/Screens/WidgetConfiguration/ViewModel/WidgetEditorViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class WidgetEditorViewModel: NSObject {
+    
+    private(set) var widgetId: UUID!
+    
+    private(set) weak var delegate: HomeNavigationFlowDelegate?
+    
+    // MARK: - Setters
+    func setDelegate(to delegate: HomeNavigationFlowDelegate) {
+        self.delegate = delegate
+    }
+    
+    func setWidgetId(to id: UUID) {
+        self.widgetId = id
+    }
+    
+}
