72d11d6d3b5f02afce40f38ee9753255f296a200
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-08-21 17:27:49 -0300

    Created cell that will be used to customize the module style. This will be improved later.

--- Modulite/Screens/WidgetConfiguration/View/Editor/CollectionViewCells/ModuleStyleCell.swift
+++ Modulite/Screens/WidgetConfiguration/View/Editor/CollectionViewCells/ModuleStyleCell.swift
@@ -8,5 +8,30 @@
 import UIKit
 
 class ModuleStyleCell: UICollectionViewCell {
+    static let reuseId = "ModuleStyleCell"
     
+    // MARK: - Properties
+    
+    private(set) lazy var styleImageView: UIImageView = {
+        let view = UIImageView()
+        
+        return view
+    }()
+    
+    // MARK: - Setup
+    func setup(with image: UIImage) {
+        styleImageView.image = image
+        addSubviews()
+        setupConstraints()
+    }
+    
+    func addSubviews() {
+        addSubview(styleImageView)
+    }
+    
+    func setupConstraints() {
+        styleImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
 }
