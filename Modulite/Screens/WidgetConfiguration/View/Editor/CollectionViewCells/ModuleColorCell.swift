c1ad4aa002769030c2769a7bc3722ff1b4d30abf
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-08-21 17:28:34 -0300

    Crated the cell that will be used to selected a module's color.

--- Modulite/Screens/WidgetConfiguration/View/Editor/CollectionViewCells/ModuleColorCell.swift
+++ Modulite/Screens/WidgetConfiguration/View/Editor/CollectionViewCells/ModuleColorCell.swift
@@ -5,4 +5,36 @@
 //  Created by Gustavo Munhoz Correa on 21/08/24.
 //
 
-import Foundation
+import UIKit
+
+class ModuleColorCell: UICollectionViewCell {
+    static let reuseId = "ModuleColorCell"
+    
+    // MARK: - Properties
+    private(set) lazy var circleLayer: CAShapeLayer = {
+        let circle = CAShapeLayer()
+        circle.path = UIBezierPath(ovalIn: CGRect(origin: .zero, size: frame.size)).cgPath
+        
+        return circle
+    }()
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addLayers()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup methods
+    func addLayers() {
+        layer.addSublayer(circleLayer)
+    }
+    
+    func setup(with color: UIColor) {
+        circleLayer.fillColor = color.cgColor
+    }
+}
