b29074dc703b7e2d2dd140cd1c8646f2fe5baa46
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-08-22 17:05:07 -0300

    Created `ImageProcessingFactory`, which for now is responsible to creating images with blend modes on them.

--- Modulite/Factories/ImageProcessingFactory.swift
+++ Modulite/Factories/ImageProcessingFactory.swift
@@ -5,4 +5,25 @@
 //  Created by Gustavo Munhoz Correa on 22/08/24.
 //
 
-import Foundation
+import UIKit
+
+class ImageProcessingFactory {
+    
+    static func createColorBlendedImage(
+        _ image: UIImage,
+        mode: CGBlendMode,
+        color: UIColor
+    ) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(image.size, false, image.scale)
+        image.draw(in: CGRect(origin: .zero, size: image.size))
+        
+        color.setFill()
+        let rect = CGRect(origin: .zero, size: image.size)
+        UIRectFillUsingBlendMode(rect, mode)
+        let blendedImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return blendedImage
+    }    
+}
