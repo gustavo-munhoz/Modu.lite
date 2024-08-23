836658703c28f8716dd7aec476c38dd9762f833f
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-08-23 17:25:18 -0300

    Created `WidgetStyle`, which will be used to define which modules and colors are available for each style.

--- Modulite/Models/WidgetStyle.swift
+++ Modulite/Models/WidgetStyle.swift
@@ -5,4 +5,20 @@
 //  Created by Gustavo Munhoz Correa on 23/08/24.
 //
 
-import Foundation
+import UIKit
+
+class WidgetStyle {
+    var name: String
+    var styles: [ModuleStyle]
+    var colors: [UIColor]
+
+    init(name: String, styles: [ModuleStyle], colors: [UIColor]) {
+        self.name = name
+        self.styles = styles
+        self.colors = colors
+    }
+}
+
+enum WidgetStyleKey {
+    case analog
+}
