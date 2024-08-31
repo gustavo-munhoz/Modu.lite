ecb74360ff7cfa3389e96b8624bed10c498381e2
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-08-30 22:34:27 -0300

    Added `ModuleStyleKey` to create objects based on keys.

--- Modulite/Models/Widget/ModuleStyle.swift
+++ Modulite/Models/Widget/ModuleStyle.swift
@@ -5,4 +5,36 @@
 //  Created by Gustavo Munhoz Correa on 29/08/24.
 //
 
-import Foundation
+import UIKit
+import SwiftData
+
+/// Represents the style of a module, allowing the choice from multiple styles.
+class ModuleStyle {
+    let id = UUID()
+    unowned var widgetStyle: WidgetStyle
+    var image: UIImage
+    
+    /// Initializes a new style with an optional image.
+    init(from style: WidgetStyle, imageName: String) {
+        self.widgetStyle = style
+        self.image = UIImage(named: imageName)!
+    }
+    
+    init(from style: WidgetStyle, key: ModuleStyleKey) {
+        self.widgetStyle = style
+        self.image = key.getModuleImage()
+    }
+}
+
+enum ModuleStyleKey: String, Codable {
+    case analogEmpty = "analog-empty"
+    case analogKnob = "analog-knob"
+    case analogRegular = "analog-regular"
+    case analogScreen = "analog-screen"
+    case analogSound = "analog-sound"
+    case analogSwitch = "analog-switch"
+    
+    func getModuleImage() -> UIImage {
+        UIImage(named: self.rawValue)!
+    }
+}
