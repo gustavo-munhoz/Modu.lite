b1958c4f568edbee6c29008c53e058ba6ffa5b94
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-09-02 23:30:58 -0300

    Created transformer for persisting `UIColor`.

--- Modulite/Extensions/UIColorValueTransformer.swift
+++ Modulite/Extensions/UIColorValueTransformer.swift
@@ -5,4 +5,35 @@
 //  Created by Gustavo Munhoz Correa on 02/09/24.
 //
 
-import Foundation
+import UIKit
+
+@objc(UIColorValueTransformer)
+final class UIColorValueTransformer: ValueTransformer {
+
+    override static func transformedValueClass() -> AnyClass {
+        return UIColor.self
+    }
+
+    // return data
+    override func transformedValue(_ value: Any?) -> Any? {
+        guard let color = value as? UIColor else { return nil }
+        do {
+            let data = try NSKeyedArchiver.archivedData(withRootObject: color, requiringSecureCoding: true)
+            return data
+        } catch {
+            return nil
+        }
+    }
+
+    // return UIColor
+    override func reverseTransformedValue(_ value: Any?) -> Any? {
+        guard let data = value as? Data else { return nil }
+    
+        do {
+            let color = try NSKeyedUnarchiver.unarchivedObject(ofClass: UIColor.self, from: data)
+            return color
+        } catch {
+            return nil
+        }
+    }
+}
