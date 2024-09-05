1565cea069b1799e3cb32a584b687a9603b4081b
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-09-05 16:12:57 -0300

    Created tag-like layout for `selectedAppsCollectionView`.

--- Modulite/Screens/WidgetConfiguration/View/Setup/CollectionViewLayouts/WidgetSetupAppsTagFlowLayout.swift
+++ Modulite/Screens/WidgetConfiguration/View/Setup/CollectionViewLayouts/WidgetSetupAppsTagFlowLayout.swift
@@ -5,4 +5,29 @@
 //  Created by Gustavo Munhoz Correa on 04/09/24.
 //
 
-import Foundation
+import UIKit
+
+class WidgetSetupAppsTagFlowLayout: UICollectionViewFlowLayout {
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        let attributes = super.layoutAttributesForElements(
+            in: rect
+            // swiftlint:disable:next force_cast
+        )?.map { $0.copy() as! UICollectionViewLayoutAttributes }
+        
+        var leftMargin = sectionInset.left
+        var maxY: CGFloat = -1.0
+        attributes?.forEach { layoutAttribute in
+            if layoutAttribute.frame.origin.y >= maxY {
+                leftMargin = sectionInset.left
+            }
+            
+            layoutAttribute.frame.origin.x = leftMargin
+            
+            leftMargin += layoutAttribute.frame.width + minimumInteritemSpacing
+            maxY = max(layoutAttribute.frame.maxY, maxY)
+        }
+        
+        return attributes
+    }
+}
