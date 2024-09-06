b365709867063427cbc44515afbb189e75be87e4
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-09-06 15:25:10 -0300

    Implemented init with weight and italic option through bool

--- Modulite/Extensions/UIFont+TraitedTextStyle.swift
+++ Modulite/Extensions/UIFont+TraitedTextStyle.swift
@@ -14,10 +14,10 @@ extension UIFont {
     ) {
         self.init(
             descriptor:
-                .preferredFontDescriptor(withTextStyle: textStyle)
-                .addingAttributes([.traits: [
-                    UIFontDescriptor.TraitKey.weight: weight
-                ]]),
+                    .preferredFontDescriptor(withTextStyle: textStyle)
+                    .addingAttributes([.traits: [
+                        UIFontDescriptor.TraitKey.weight: weight
+                    ]]),
             size: 0
         )
     }
@@ -31,6 +31,26 @@ extension UIFont {
         guard let descriptor = baseDescriptor.withSymbolicTraits(symbolicTraits) else {
             return nil
         }
+        
+        self.init(descriptor: descriptor, size: 0)
+    }
+    
+    convenience init(
+        textStyle: UIFont.TextStyle,
+        weight: UIFont.Weight,
+        italic: Bool
+    ) {
+        var traits: [UIFontDescriptor.TraitKey: Any] = [
+            UIFontDescriptor.TraitKey.weight: weight
+        ]
+        
+        if italic {
+            traits[UIFontDescriptor.TraitKey.symbolic] = UIFontDescriptor.SymbolicTraits.traitItalic.rawValue
+        }
+        
+        let descriptor = UIFontDescriptor
+            .preferredFontDescriptor(withTextStyle: textStyle)
+            .addingAttributes([.traits: traits])
         
         self.init(descriptor: descriptor, size: 0)
     }
