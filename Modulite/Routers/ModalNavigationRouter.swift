8a1a4850b6c5a8e3d02bb0dbcd0cbe6f99db1320
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-09-05 21:37:22 -0300

    Created router for handling modal presenting.

--- Modulite/Routers/ModalNavigationRouter.swift
+++ Modulite/Routers/ModalNavigationRouter.swift
@@ -5,4 +5,59 @@
 //  Created by Gustavo Munhoz Correa on 05/09/24.
 //
 
-import Foundation
+import UIKit
+
+class ModalNavigationRouter: NSObject {
+    unowned let parentViewController: UIViewController
+    private let navigationController = UINavigationController()
+    private var onDismissForViewController: [UIViewController: (() -> Void)] = [:]
+    
+    init(parentViewController: UIViewController) {
+        self.parentViewController = parentViewController
+        super.init()
+        self.navigationController.delegate = self
+    }
+}
+
+extension ModalNavigationRouter: Router {
+    func present(_ viewController: UIViewController, animated: Bool, onDismiss: (() -> Void)?) {
+        onDismissForViewController[viewController] = onDismiss
+        
+        if navigationController.viewControllers.isEmpty {
+            presentModally(viewController, animated: animated)
+            
+        } else {
+            navigationController.pushViewController(viewController, animated: animated)
+        }
+    }
+    
+    private func presentModally(_ viewController: UIViewController, animated: Bool) {
+        navigationController.setViewControllers([viewController], animated: false)
+        parentViewController.present(navigationController, animated: animated)
+    }
+    
+    func dismiss(animated: Bool) {
+        performOnDismiss(for: navigationController.viewControllers.first!)
+        parentViewController.dismiss(animated: animated)
+    }
+    
+    private func performOnDismiss(for viewController: UIViewController) {
+        guard let onDismiss = onDismissForViewController[viewController] else { return }
+        
+        onDismiss()
+        onDismissForViewController[viewController] = nil
+    }
+}
+
+extension ModalNavigationRouter: UINavigationControllerDelegate {
+    func navigationController(
+        _ navigationController: UINavigationController,
+        didShow viewController: UIViewController,
+        animated: Bool
+    ) {
+        guard let dismissedVC = navigationController.transitionCoordinator?.viewController(forKey: .from),
+              !navigationController.viewControllers.contains(dismissedVC) else { return }
+        
+        performOnDismiss(for: dismissedVC)
+    }
+}
