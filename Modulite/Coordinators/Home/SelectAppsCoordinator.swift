c9402334d2a01ec263b3135be72d3da4018827e4
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-09-05 21:37:54 -0300

    Implemented coordinator for handling app searching.

--- Modulite/Coordinators/Home/SelectAppsCoordinator.swift
+++ Modulite/Coordinators/Home/SelectAppsCoordinator.swift
@@ -5,4 +5,19 @@
 //  Created by Gustavo Munhoz Correa on 05/09/24.
 //
 
-import Foundation
+import UIKit
+
+class SelectAppsCoordinator: Coordinator {
+    var children: [Coordinator] = []
+    
+    var router: Router
+    
+    init(router: Router) {
+        self.router = router
+    }
+    
+    func present(animated: Bool, onDismiss: (() -> Void)?) {
+        let viewController = SelectAppsViewController()
+        router.present(viewController, animated: animated, onDismiss: onDismiss)
+    }
+}
