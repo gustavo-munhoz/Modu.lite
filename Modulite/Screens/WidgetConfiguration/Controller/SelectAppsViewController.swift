f9377417857c4cfc8f3e69d13f59cde2f47e9974
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-09-05 21:41:45 -0300

    Created files that will be used to define search apps modal.

--- Modulite/Screens/WidgetConfiguration/Controller/SelectAppsViewController.swift
+++ Modulite/Screens/WidgetConfiguration/Controller/SelectAppsViewController.swift
@@ -5,4 +5,21 @@
 //  Created by Gustavo Munhoz Correa on 05/09/24.
 //
 
-import Foundation
+import UIKit
+
+class SelectAppsViewController: UIViewController {
+    
+    // MARK: - Properties
+    private var selectAppsView = SelectAppsView()
+    
+    // MARK: - Lifecycle
+    
+    override func loadView() {
+        view = selectAppsView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
--- Modulite/Screens/WidgetConfiguration/View/Setup/SelectApps/SelectAppsView.swift
+++ Modulite/Screens/WidgetConfiguration/View/Setup/SelectApps/SelectAppsView.swift
@@ -5,4 +5,26 @@
 //  Created by Gustavo Munhoz Correa on 05/09/24.
 //
 
-import Foundation
+import UIKit
+
+class SelectAppsView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubviews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubviews() {
+        
+    }
+    
+    private func setupConstraints() {
+        
+    }
+}
+
