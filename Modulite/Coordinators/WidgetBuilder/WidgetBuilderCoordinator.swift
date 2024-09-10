d2e28de664a089635d588d6100890665455ccb06
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-09-10 16:38:02 -0300

    WidgetBuilderCoordinator holds a builder that is shared through views

--- Modulite/Coordinators/WidgetBuilder/WidgetBuilderCoordinator.swift
+++ Modulite/Coordinators/WidgetBuilder/WidgetBuilderCoordinator.swift
@@ -5,4 +5,67 @@
 //  Created by Gustavo Munhoz Correa on 09/09/24.
 //
 
-import Foundation
+import UIKit
+
+class WidgetBuilderCoordinator: Coordinator {
+    let builder = WidgetConfigurationBuilder()
+    
+    var children: [Coordinator] = []
+    
+    var router: Router
+    
+    init(router: Router) {
+        self.router = router
+    }
+    
+    func present(animated: Bool, onDismiss: (() -> Void)?) {
+        let viewController = WidgetSetupViewController.instantiate(widgetId: UUID(), delegate: self)
+        viewController.hidesBottomBarWhenPushed = true
+        
+        router.present(viewController, animated: animated, onDismiss: onDismiss)
+    }
+}
+
+extension WidgetBuilderCoordinator: WidgetSetupViewControllerDelegate {
+    func widgetSetupViewControllerDidSelectWidgetStyle(
+        _ controller: WidgetSetupViewController,
+        style: WidgetStyle
+    ) {
+        builder.setWidgetStyle(style)
+    }
+    
+    func widgetSetupViewControllerDidTapSearchApps(
+        _ parentController: WidgetSetupViewController
+    ) {
+        let router = ModalNavigationRouter(parentViewController: parentController)
+        
+        let coordinator = SelectAppsCoordinator(
+            delegate: self,
+            selectedApps: builder.getCurrentApps(),
+            router: router
+        )
+        
+        presentChild(coordinator, animated: true) { [weak self] in
+            guard let self = self else { return }
+            parentController.didFinishSelectingApps(
+                apps: self.builder.getCurrentApps()
+            )
+        }
+    }
+}
+
+extension WidgetBuilderCoordinator: SelectAppsViewControllerDelegate {
+    func selectAppsViewControllerDidSelectApp(
+        _ controller: SelectAppsViewController,
+        didSelect app: AppInfo
+    ) {
+        builder.addSelectedApp(for: app)
+    }
+    
+    func selectAppsViewControllerDidDeselectApp(
+        _ controller: SelectAppsViewController,
+        didDeselect app: AppInfo
+    ) {
+        builder.removeSelectedApp(for: app)
+    }
+}
