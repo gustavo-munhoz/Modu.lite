dbaff58351ccae88821e3ae6629daf8a831f8280
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-09-13 22:09:54 -0300

    Added names to widgets and implemented hightlighted states

--- Modulite/Screens/Home/View/CollectionViewCells/MainWidgetCollectionViewCell.swift
+++ Modulite/Screens/Home/View/CollectionViewCells/MainWidgetCollectionViewCell.swift
@@ -11,33 +11,62 @@ import SnapKit
 class MainWidgetCollectionViewCell: UICollectionViewCell {
     static let reuseId = "MainWidgetCollectionViewCell"
     
-    // Create a single UIImageView instead of a stack view
-    let imageView = UIImageView()
+    // MARK: - Properties
+    override var isHighlighted: Bool {
+        didSet {
+            toggleIsHighlighted()
+        }
+    }
     
+    private(set) lazy var widgetImageView: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFit
+        view.clipsToBounds = true
+        
+        return view
+    }()
+    
+    private(set) lazy var widgetNameLabel: UILabel = {
+        let view = UILabel()
+        view.font = UIFont(textStyle: .callout, weight: .semibold)
+        view.textColor = .systemGray
+        view.textAlignment = .center
+        
+        return view
+    }()
+    
+    // MARK: - Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupImageView()
+        
+        addSubviews()
+        setupConstraints()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    private func setupImageView() {
-        imageView.contentMode = .scaleAspectFit
-        imageView.clipsToBounds = true
-        contentView.addSubview(imageView)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
-        ])
+    // MARK: - Setup methods
+    func configure(image: UIImage?, name: String?) {
+        widgetImageView.image = image
+        widgetNameLabel.text = name
     }
     
-    func configure(with image: UIImage?) {
-        imageView.image = image
+    private func addSubviews() {
+        addSubview(widgetImageView)
+        addSubview(widgetNameLabel)
+    }
+    
+    private func setupConstraints() {
+        widgetImageView.snp.makeConstraints { make in
+            make.top.left.right.equalToSuperview()
+            make.height.equalToSuperview().offset(-38)
+        }
+        
+        widgetNameLabel.snp.makeConstraints { make in
+            make.left.right.equalToSuperview()
+            make.top.equalTo(widgetImageView.snp.bottom).offset(16)
+        }
     }
 }
