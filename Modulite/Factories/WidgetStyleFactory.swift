d5c9bd457448217fa406d7b1fc0dc9e43914262b
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-09-23 17:44:03 -0300

    Refactored to have a function for each style

--- Modulite/Factories/WidgetStyleFactory.swift
+++ Modulite/Factories/WidgetStyleFactory.swift
@@ -13,63 +13,75 @@ class WidgetStyleFactory {
     static func styleForKey(_ key: WidgetStyleKey) -> WidgetStyle {
         switch key {
         case .analog:
-            let textConfig = ModuleAppNameTextConfiguration()
-                .font(UIFont(textStyle: .caption2, weight: .semibold))
-                .textColor(.iceCold)
-                .shadow(color: .iceCold, blurRadius: 5)
-                .textCase(.lower)
-                .removingSpaces()
-                .preffix(".")
-
-            let style = WidgetStyle(
-                key: .analog,
-                name: .localized(for: .widgetStyleNameAnalog),
-                coverImage: UIImage(named: "analog-style-cover")!,
-                background: .color(.black),
-                colors: [.white, .eggYolk, .cupcake, .sweetTooth, .sugarMint, .burntEnds],
-                textConfiguration: textConfig
-            )
-            
-            let moduleStyles = [
-                ModuleStyle(from: style, key: .analogRegular),
-                ModuleStyle(from: style, key: .analogKnob),
-                ModuleStyle(from: style, key: .analogScreen),
-                ModuleStyle(from: style, key: .analogSound),
-                ModuleStyle(from: style, key: .analogSwitch)
-            ]
-
-            style.setModuleStyles(to: moduleStyles)
-            style.setEmptyStyle(to: ModuleStyle(from: style, key: .analogEmpty))
-            
-            return style
+            return createAnalogStyle()
             
         case .tapedeck:
-            let textConfig = ModuleAppNameTextConfiguration()
-                .font(UIFont(textStyle: .caption2, weight: .semibold))
-                .textColor(.white)
-                .textCase(.upper)
-            
-            let style = WidgetStyle(
-                key: .tapedeck,
-                name: .localized(for: .widgetStyleNameTapedeck),
-                coverImage: UIImage(named: "tapedeck-style-cover")!,
-                background: .color(.black),
-                textConfiguration: textConfig
-            )
-            
-            let moduleStyles = [
-                ModuleStyle(from: style, key: .tapedeck8seg),
-                ModuleStyle(from: style, key: .tapedeckPower),
-                ModuleStyle(from: style, key: .tapedeckSelector),
-                ModuleStyle(from: style, key: .tapedeckSlider),
-                ModuleStyle(from: style, key: .tapedeckSound)
-            ]
-            
-            style.setModuleStyles(to: moduleStyles)
-            
-            // FIXME: Need to set empty style
-            
-            return style
+            return createTapedeckStyle()
         }
+    }
+    
+    private static func createAnalogStyle() -> WidgetStyle {
+        let textConfig = ModuleAppNameTextConfiguration()
+            .font(UIFont(textStyle: .caption2, weight: .semibold))
+            .textColor(.iceCold)
+            .shadow(color: .iceCold, blurRadius: 5)
+            .textCase(.lower)
+            .removingSpaces()
+            .preffix(".")
+
+        let style = WidgetStyle(
+            key: .analog,
+            name: .localized(for: .widgetStyleNameAnalog),
+            previewImage: UIImage(named: "analogPreview")!,
+            background: .color(.black),
+            colors: [.white, .eggYolk, .cupcake, .sweetTooth, .sugarMint, .burntEnds],
+            textConfiguration: textConfig,
+            blockedScreenWallpaperImage: UIImage(named: "analogWallpaper")!,
+            homeScreenWallpaperImage: UIImage(named: "analogWallpaper")!
+        )
+        
+        let moduleStyles = [
+            ModuleStyle(from: style, key: .analogRegular),
+            ModuleStyle(from: style, key: .analogKnob),
+            ModuleStyle(from: style, key: .analogScreen),
+            ModuleStyle(from: style, key: .analogSound),
+            ModuleStyle(from: style, key: .analogSwitch)
+        ]
+
+        style.setModuleStyles(to: moduleStyles)
+        style.setEmptyStyle(to: ModuleStyle(from: style, key: .analogEmpty))
+        
+        return style
+    }
+    
+    private static func createTapedeckStyle() -> WidgetStyle {
+        let textConfig = ModuleAppNameTextConfiguration()
+            .font(UIFont(textStyle: .caption2, weight: .semibold))
+            .textColor(.white)
+            .textCase(.upper)
+        
+        let style = WidgetStyle(
+            key: .tapedeck,
+            name: .localized(for: .widgetStyleNameTapedeck),
+            previewImage: UIImage(named: "tapedeckPreview")!,
+            background: .color(.black),
+            textConfiguration: textConfig,
+            blockedScreenWallpaperImage: UIImage(named: "tapedeckWallpaper")!,
+            homeScreenWallpaperImage: UIImage(named: "tapedeckWallpaper")!
+        )
+        
+        let moduleStyles = [
+            ModuleStyle(from: style, key: .tapedeck8seg),
+            ModuleStyle(from: style, key: .tapedeckPower),
+            ModuleStyle(from: style, key: .tapedeckSelector),
+            ModuleStyle(from: style, key: .tapedeckSlider),
+            ModuleStyle(from: style, key: .tapedeckSound)
+        ]
+        
+        style.setModuleStyles(to: moduleStyles)
+        
+        // FIXME: Need to set empty style
+        
+        return style
     }
 }
