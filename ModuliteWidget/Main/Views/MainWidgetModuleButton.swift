65aaf0012c70d8a4ecc54df3a14c86cedd2dc6c2
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-09-25 15:01:43 -0300

    Created view for a module button

--- ModuliteWidget/Main/Views/MainWidgetModuleButton.swift
+++ ModuliteWidget/Main/Views/MainWidgetModuleButton.swift
@@ -5,4 +5,26 @@
 //  Created by Gustavo Munhoz Correa on 24/09/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct MainWidgetModuleButton: View {
+    var moduleImage: Image
+    var stringURL: String?
+    
+    var body: some View {
+        Group {
+            if let stringURL = stringURL {
+                Link(destination: URL(string: "moduliteap://app?app=\(stringURL)")!) {
+                    moduleImage
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                }
+                
+            } else {
+                moduleImage
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+        }
+    }
+}
