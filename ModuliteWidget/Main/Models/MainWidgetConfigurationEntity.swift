09800f543e3fa4dbd4eb969fbd89f50bcb5b2b9b
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-09-26 15:30:16 -0300

    Created new files for widget intent configuration

---
+++ ModuliteWidget/Main/Models/MainWidgetConfigurationEntity.swift
@@ -0,0 +1,8 @@
+//
+//  MainWidgetConfigurationEntity.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 25/09/24.
+//
+
+import Foundation
---
+++ ModuliteWidget/Main/Models/MainWidgetConfigurationQuery.swift
@@ -0,0 +1,8 @@
+//
+//  MainWidgetConfigurationQuery.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 25/09/24.
+//
+
+import Foundation
