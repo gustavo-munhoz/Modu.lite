73f146a7a9b3ea2a0aae1bf06a39ff4aa3c040bc
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-09-26 15:34:40 -0300

    Created intent for selecting main widget

--- ModuliteWidget/Main/Intents/SelectMainWidgetConfigurationIntent.swift
+++ ModuliteWidget/Main/Intents/SelectMainWidgetConfigurationIntent.swift
@@ -5,4 +5,19 @@
 //  Created by Gustavo Munhoz Correa on 25/09/24.
 //
 
-import Foundation
+import AppIntents
+
+struct SelectMainWidgetConfigurationIntent: WidgetConfigurationIntent {
+    // TODO: Use default localizable key pattern
+    static var title: LocalizedStringResource = "Select Widget Configuration"
+    static var description = IntentDescription("Selects the widget configuration to display.")
+
+    @Parameter(title: "Widget Configuration")
+    var widgetConfiguration: MainWidgetConfigurationEntity?
+
+    init() {}
+
+    init(widgetConfiguration: MainWidgetConfigurationEntity) {
+        self.widgetConfiguration = widgetConfiguration
+    }
+}
