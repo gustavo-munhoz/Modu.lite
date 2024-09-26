408a0ccf9c8252431e2c985639bc3e7ee151c7b8
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-09-26 15:36:41 -0300

    Created MainWidget `AppEntity`

--- ModuliteWidget/Main/Models/MainWidgetConfigurationEntity.swift
+++ ModuliteWidget/Main/Models/MainWidgetConfigurationEntity.swift
@@ -5,4 +5,21 @@
 //  Created by Gustavo Munhoz Correa on 25/09/24.
 //
 
-import Foundation
+import AppIntents
+import CoreData
+
+struct MainWidgetConfigurationEntity: AppEntity {
+    var id: UUID
+    var name: String
+    
+    // TODO: Use default localizable key pattern
+    static var typeDisplayRepresentation: TypeDisplayRepresentation = "Main Widget Configuration"
+
+    var displayRepresentation: DisplayRepresentation {
+        DisplayRepresentation(
+            title: "\(name)"
+        )
+    }
+    
+    static var defaultQuery = MainWidgetConfigurationQuery()
+}
