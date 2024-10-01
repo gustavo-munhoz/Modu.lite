066fa672fe59b4131ebea7058196806105f266ba
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-01 16:00:03 -0300

    Separated WidgetEditorVC in multiple files to reduce file size

---
+++ Modulite/Screens/WidgetConfiguration/Editor/ViewController/Extensions/WidgetEditorViewController+UICollectionViewDataSource.swift
@@ -0,0 +1,82 @@
+//
+//  WidgetEditorViewController+UICollectionViewDataSource.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 01/10/24.
+//
+
+import UIKit
+
+extension WidgetEditorViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch collectionView {
+        case editorView.widgetLayoutCollectionView: return viewModel.getCurrentModules().count
+        case editorView.moduleStyleCollectionView: return viewModel.getAvailableStyles().count
+        case editorView.moduleColorCollectionView: return viewModel.getAvailableColors().count
+        default: return 0
+        }
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        
+        switch collectionView {
+        case editorView.widgetLayoutCollectionView:
+            // MARK: - Create cells for widget
+            return handleLayoutCellCreation(for: collectionView, indexPath: indexPath)
+
+        case editorView.moduleStyleCollectionView:
+            // MARK: - Create cells for module styles
+            guard let style = viewModel.getAvailableStyle(at: indexPath.row),
+                  let cell = collectionView.dequeueReusableCell(
+                    withReuseIdentifier: ModuleStyleCell.reuseId,
+                    for: indexPath
+                  ) as? ModuleStyleCell else {
+                fatalError("Could not dequeue ModuleStyleCell.")
+            }
+            
+            cell.setup(with: style)
+            return cell
+            
+        case editorView.moduleColorCollectionView:
+            // MARK: - Create cells for colors
+            guard let color = viewModel.getAvailableColor(at: indexPath.row),
+                  let cell = collectionView.dequeueReusableCell(
+                    withReuseIdentifier: ModuleColorCell.reuseId,
+                    for: indexPath
+                  ) as? ModuleColorCell else {
+                fatalError("Could not dequeue ModuleColorCell.")
+            }
+            
+            cell.setup(with: color)
+            return cell
+            
+        default:
+            fatalError("Unsupported `UICollectionView`.")
+        }
+    }
+    
+    private func handleLayoutCellCreation(
+        for collectionView: UICollectionView,
+        indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let module = viewModel.getModule(at: indexPath.row),
+              let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: WidgetModuleCell.reuseId,
+            for: indexPath
+        ) as? WidgetModuleCell else {
+            fatalError("Could not dequeue WidgetModuleCell.")
+        }
+        
+        if let index = viewModel.selectedCellIndex {
+            cell.setEditable(index == indexPath.row)
+        } else {
+            cell.startWiggling()
+        }
+        
+        cell.setup(with: module)
+        return cell
+    }
+}
---
+++ Modulite/Screens/WidgetConfiguration/Editor/ViewController/Extensions/WidgetEditorViewController+UICollectionViewDragAndDropDelegate.swift
@@ -0,0 +1,72 @@
+//
+//  WidgetEditorViewController+UICollectionViewDragAndDropDelegate.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 01/10/24.
+//
+
+import UIKit
+
+// MARK: - UICollectionViewDragDelegate
+extension WidgetEditorViewController: UICollectionViewDragDelegate {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        itemsForBeginning session: any UIDragSession,
+        at indexPath: IndexPath
+    ) -> [UIDragItem] {
+        guard let item = viewModel.getModule(at: indexPath.row),
+              let image = item.resultingImage
+        else { return [] }
+        
+        let itemProvider = NSItemProvider(object: image as UIImage)
+        let dragItem = UIDragItem(itemProvider: itemProvider)
+        dragItem.localObject = item
+        
+        return [dragItem]
+    }
+}
+
+// MARK: - UICollectionViewDropDelegate
+extension WidgetEditorViewController: UICollectionViewDropDelegate {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        canHandle session: any UIDropSession
+    ) -> Bool {
+        return session.canLoadObjects(ofClass: UIImage.self)
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        dropSessionDidUpdate session: any UIDropSession,
+        withDestinationIndexPath destinationIndexPath: IndexPath?
+    ) -> UICollectionViewDropProposal {
+        return UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        performDropWith coordinator: any UICollectionViewDropCoordinator
+    ) {
+        guard let destinationIndexPath = coordinator.destinationIndexPath else { return }
+        
+        switch coordinator.proposal.operation {
+        case .move:
+            let items = coordinator.items
+            if items.count == 1, let item = items.first, let sourceIndexPath = item.sourceIndexPath {
+                collectionView.performBatchUpdates { [weak self] in
+                    guard let viewModel = self?.viewModel
+                    else { return }
+                    
+                    viewModel.moveItem(from: sourceIndexPath.item, to: destinationIndexPath.item)
+                    
+                    collectionView.deleteItems(at: [sourceIndexPath])
+                    collectionView.insertItems(at: [destinationIndexPath])
+                }
+                
+                coordinator.drop(items.first!.dragItem, toItemAt: destinationIndexPath)
+            }
+        default:
+            return
+        }
+    }
+}
