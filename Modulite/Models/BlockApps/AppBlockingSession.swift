0554ce431c9fd5600e2226cc7597b287d5601d41
Andre <andre_fabriciow@hotmail.com>
2024-10-04 16:03:08 -0300

    Refactoring AppBlockingSession to use AppBlockManager and making sessions is unique

--- Modulite/Models/BlockApps/AppBlockingSession.swift
+++ Modulite/Models/BlockApps/AppBlockingSession.swift
@@ -7,8 +7,10 @@
 
 import Foundation
 import FamilyControls
+import DeviceActivity
 
 class AppBlockingSession {
+    var id = UUID()
     var name: String?
     var selection: FamilyActivitySelection!
     var blockingType: BlockType?
@@ -17,6 +19,10 @@ class AppBlockingSession {
     var endsAt: DateComponents?
     var daysOfWeek: [WeekDay] = []
     var isActive: Bool
+    
+    private var blockManager: AppBlockManager
+    private var activityName: DeviceActivityName
+    private var schedule: DeviceActivitySchedule
     
     var time: String {
         let startFormatted = formatTime(from: startsAt)
@@ -27,7 +33,7 @@ class AppBlockingSession {
     var appsBlocked: Int {
         return selection.applications.count
     }
-    
+
     init(
         name: String,
         selection: FamilyActivitySelection,
@@ -46,11 +52,34 @@ class AppBlockingSession {
         self.endsAt = endsAt
         self.daysOfWeek = daysOfWeek
         self.isActive = isActive
+        
+        self.activityName = DeviceActivityName("block_\(UUID().uuidString)")
+        
+        self.schedule = DeviceActivitySchedule(
+            intervalStart: startsAt,
+            intervalEnd: endsAt,
+            repeats: true
+        )
+        
+        self.blockManager = AppBlockManager(
+            selection: selection,
+            activityName: self.activityName,
+            schedule: self.schedule
+        )
     }
+    
     private func formatTime(from components: DateComponents?) -> String {
         guard let hour = components?.hour, let minute = components?.minute else {
             return "00:00"
         }
         return String(format: "%02d:%02d", hour, minute)
+    }
+    
+    func activateBlock() {
+        blockManager.startBlock()
+    }
+
+    func deactivateBlock() {
+        blockManager.stopBlock()
     }
 }
