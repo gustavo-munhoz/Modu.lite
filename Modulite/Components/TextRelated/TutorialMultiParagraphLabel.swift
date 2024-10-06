d992d2c814e79296f5cb9115cc6e16a3060b68e6
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-05 22:18:49 -0300

    Created text components to facilitate tutorial view construction

--- Modulite/Components/TextRelated/TutorialMultiParagraphLabel.swift
+++ Modulite/Components/TextRelated/TutorialMultiParagraphLabel.swift
@@ -5,4 +5,21 @@
 //  Created by Gustavo Munhoz Correa on 04/10/24.
 //
 
-import Foundation
+import UIKit
+
+class TutorialMultiParagraphLabel: UIStackView {
+    convenience init(localizedKeys: [TutorialEditWidgetLocalizedTexts]) {
+        var paragraphs: [TutorialTextParagraphLabel] = []
+        
+        for key in localizedKeys {
+            let paragraph = TutorialTextParagraphLabel(textLocalizedKey: key)
+            paragraphs.append(paragraph)
+        }
+        
+        self.init(arrangedSubviews: paragraphs)
+        
+        axis = .vertical
+        spacing = 20
+        distribution = .equalSpacing
+    }
+}
