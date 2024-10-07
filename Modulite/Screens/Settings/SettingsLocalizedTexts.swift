946173182f3ac266e2ac524b33116f592c1b402f
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-07 17:45:47 -0300

    Created and moved files

---
+++ Modulite/Screens/Settings/SettingsLocalizedTexts.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsLocalizedTexts.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 07/10/24.
+//
+
+import Foundation
---
+++ Modulite/Screens/Settings/View/PreferenceTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  PreferenceTableViewCell.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 07/10/24.
+//
+
+import Foundation
