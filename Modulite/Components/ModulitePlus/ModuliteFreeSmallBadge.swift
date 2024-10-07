2982549616f640e9a71e231f8e54dc31774052d7
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-07 17:47:21 -0300

    Created badge for free version

--- Modulite/Components/ModulitePlus/ModuliteFreeSmallBadge.swift
+++ Modulite/Components/ModulitePlus/ModuliteFreeSmallBadge.swift
@@ -5,4 +5,32 @@
 //  Created by Gustavo Munhoz Correa on 07/10/24.
 //
 
-import Foundation
+import UIKit
+
+class ModuliteFreeSmallBadge: GradientLabelView {
+    convenience init() {
+        self.init(
+            gradient: Gradient(
+                colors: [.potatoYellow, .potatoYellow],
+                direction: CGVector(dx: 1, dy: 0)
+            ),
+            insets: .init(vertical: 5, horizontal: 5)
+        )
+        
+        textAlignment = .center
+        attributedText = NSAttributedString(
+            string: .localized(for: .free),
+            attributes: [
+                .foregroundColor: UIColor.textPrimary,
+                .font: UIFont.systemFont(
+                    ofSize: 16,
+                    weight: .heavy,
+                    width: .expanded
+                )
+            ]
+        )
+        
+        layer.cornerRadius = 5
+        clipsToBounds = true
+    }
+}
