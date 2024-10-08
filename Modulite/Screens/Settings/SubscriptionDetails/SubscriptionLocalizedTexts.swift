5d81955f54cf67f09c919ba5e147c5a5385a572d
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-08 10:43:48 -0300

    Created and reorganized settings related files

---
+++ Modulite/Screens/Settings/SubscriptionDetails/SubscriptionLocalizedTexts.swift
@@ -0,0 +1,8 @@
+//
+//  SubscriptionLocalizedTexts.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 07/10/24.
+//
+
+import Foundation
---
+++ Modulite/Screens/Settings/SubscriptionDetails/View/SubscriptionDetailsView.swift
@@ -0,0 +1,8 @@
+//
+//  SubscriptionDetailsView.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 07/10/24.
+//
+
+import Foundation
