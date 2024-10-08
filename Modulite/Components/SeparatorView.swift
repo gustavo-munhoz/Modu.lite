703bbddf32921a67a4ff4fbfae4cf88a0158ab75
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-08 10:44:21 -0300

    Created separator view

--- Modulite/Components/SeparatorView.swift
+++ Modulite/Components/SeparatorView.swift
@@ -5,4 +5,27 @@
 //  Created by Gustavo Munhoz Correa on 08/10/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class SeparatorView: UIView {
+    
+    // MARK: - Initializer
+    init(backgroundColor: UIColor = .potatoYellow) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    // MARK: - Setup Method
+    private func setupView() {
+        snp.makeConstraints { make in
+            make.height.equalTo(2)
+        }
+    }
+}
