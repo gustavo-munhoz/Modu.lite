9244984109a2da4cf3db894974eedfb0e43ae6f3
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-08 10:49:25 -0300

    Implemented SubscriptionDetails view

--- Modulite/Screens/Settings/SubscriptionDetails/ViewController/SubscriptionDetailsViewController.swift
+++ Modulite/Screens/Settings/SubscriptionDetails/ViewController/SubscriptionDetailsViewController.swift
@@ -1,8 +1,63 @@
 //
-//  SubscriptionDetailsView.swift
+//  SubscriptionDetailsViewController.swift
 //  Modulite
 //
 //  Created by Gustavo Munhoz Correa on 07/10/24.
 //
 
-import Foundation
+import UIKit
+
+// swiftlint:disable:next type_name
+protocol SubscriptionDetailsViewControllerDelegate: AnyObject {
+    func subscriptionDetailsViewControllerDidPressUpgrade(
+        _ viewController: SubscriptionDetailsViewController
+    )
+}
+
+class SubscriptionDetailsViewController: UIViewController {
+    
+    // MARK: - Properties
+    private let subscriptionView = SubscriptionDetailsView()
+    
+    weak var delegate: SubscriptionDetailsViewControllerDelegate?
+    
+    // MARK: - Lifecycle
+    override func loadView() {
+        view = subscriptionView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupNavigationBar()
+        setupViewActions()
+    }
+    
+    // MARK: - Setup Methods
+    private func setupNavigationBar() {
+        navigationItem.title = .localized(
+            for: SettingsLocalizedTexts.settingsViewSubscriptionDetailsTitle
+        )
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func setupViewActions() {
+        subscriptionView.onUpgradeToPlusTapped = didPressUpgrade
+    }
+    
+    // MARK: - Actions
+    
+    func didPressUpgrade() {
+        delegate?.subscriptionDetailsViewControllerDidPressUpgrade(self)
+    }
+}
+
+extension SubscriptionDetailsViewController {
+    static func instantiate(delegate: SubscriptionDetailsViewControllerDelegate) -> Self {
+        let vc = Self()
+        vc.delegate = delegate
+        
+        return vc
+    }
+}
