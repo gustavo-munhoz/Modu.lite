4edf703d2fbca271f58366add24b769514daa843
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-08 10:49:07 -0300

    Created components for subscription details view

--- Modulite/Screens/Settings/SubscriptionDetails/View/Components/SubscriptionDetailsDefaultLabel.swift
+++ Modulite/Screens/Settings/SubscriptionDetails/View/Components/SubscriptionDetailsDefaultLabel.swift
@@ -5,4 +5,23 @@
 //  Created by Gustavo Munhoz Correa on 07/10/24.
 //
 
-import Foundation
+import UIKit
+
+class SubscriptionDetailsDefaultLabel: UILabel {
+    convenience init(localizedKey: SubscriptionLocalizedTexts) {
+        self.init(frame: .zero)
+        
+        text = .localized(for: localizedKey)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        font = UIFont(textStyle: .body, weight: .semibold)
+        textColor = .textPrimary
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
