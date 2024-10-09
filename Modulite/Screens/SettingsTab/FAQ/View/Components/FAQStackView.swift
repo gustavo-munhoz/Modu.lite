24585f382540caf9aa8aa5a7791be155676b3660
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-09 16:02:15 -0300

    Created components for FAQView

--- Modulite/Screens/SettingsTab/FAQ/View/Components/FAQStackView.swift
+++ Modulite/Screens/SettingsTab/FAQ/View/Components/FAQStackView.swift
@@ -5,4 +5,34 @@
 //  Created by Gustavo Munhoz Correa on 09/10/24.
 //
 
-import Foundation
+import UIKit
+
+class FAQStackView: UIStackView {
+
+    // MARK: - Initializers
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        axis = .vertical
+        spacing = 16
+        distribution = .equalSpacing
+    }
+
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Setup Methods
+
+    func populateQuestions(with questions: [FAQQuestionModel]) {
+        setupQuestions(questions)
+    }
+    
+    private func setupQuestions(_ questions: [FAQQuestionModel]) {
+        questions.forEach { question in
+            let questionView = FAQQuestionAndAnswerView()
+            questionView.setupQuestionAndAnswer(question: question)
+            addArrangedSubview(questionView)
+        }
+    }
+}
--- Modulite/Screens/SettingsTab/FAQ/View/Components/FAQSummaryStackView.swift
+++ Modulite/Screens/SettingsTab/FAQ/View/Components/FAQSummaryStackView.swift
@@ -5,4 +5,58 @@
 //  Created by Gustavo Munhoz Correa on 09/10/24.
 //
 
-import Foundation
+import UIKit
+
+class FAQSummaryStackView: UIStackView {
+    // MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        axis = .vertical
+        alignment = .leading
+        distribution = .equalSpacing
+        spacing = 16
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Method
+    func setupSummaryButtons(for questions: [FAQQuestionModel], target: Any?, action: Selector) {
+        arrangedSubviews.forEach { $0.removeFromSuperview() }
+        
+        for (index, question) in questions.enumerated() {
+            var config = UIButton.Configuration.plain()
+            
+            let attributedString = NSMutableAttributedString(
+                string: question.question,
+                attributes: [
+                    .font: UIFont(textStyle: .body, weight: .semibold),
+                    .foregroundColor: UIColor.carrotOrange,
+                    .underlineStyle: NSUnderlineStyle.single.rawValue,
+                    .underlineColor: UIColor.carrotOrange
+                ]
+            )
+            
+            config.titleAlignment = .leading
+            config.titleLineBreakMode = .byWordWrapping
+            config.attributedTitle = AttributedString(attributedString)
+            config.contentInsets = .zero
+            
+            let button = UIButton(configuration: config)
+            button.tag = index
+            button.addTarget(target, action: action, for: .touchUpInside)
+            
+            button.configurationUpdateHandler = { btn in
+                switch btn.state {
+                case .highlighted:
+                    btn.alpha = 0.5
+                default:
+                    btn.alpha = 1
+                }
+            }
+            
+            addArrangedSubview(button)
+        }
+    }
+}
