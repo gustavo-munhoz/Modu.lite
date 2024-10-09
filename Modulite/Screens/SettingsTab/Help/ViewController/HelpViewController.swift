e4dbec95036ae8e974673779fdb2d4c6ffffea0a
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-09 19:49:06 -0300

    Implemented HelpView, controller and model.

--- Modulite/Screens/SettingsTab/Help/ViewController/HelpViewController.swift
+++ Modulite/Screens/SettingsTab/Help/ViewController/HelpViewController.swift
@@ -5,4 +5,50 @@
 //  Created by Gustavo Munhoz Correa on 09/10/24.
 //
 
-import Foundation
+import UIKit
+
+protocol HelpViewControllerDelegate: AnyObject {
+    func helpViewControllerDidPressReportIssue(
+        _ viewController: HelpViewController
+    )
+}
+
+class HelpViewController: UIViewController {
+    // MARK: - Properties
+    private let helpView = HelpView()
+    private let viewModel = HelpViewModel()
+    
+    weak var delegate: HelpViewControllerDelegate?
+    
+    // MARK: - Lifecycle
+    override func loadView() {
+        view = helpView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = .localized(for: SettingsLocalizedTexts.settingsViewHelpTitle)
+        setupView()
+    }
+    
+    // MARK: - Setup Methods
+    private func setupView() {
+        helpView.setTopics(to: viewModel.helpTopics)
+        helpView.onReportIssuesButtonTapped = didPressReportIssue
+    }
+    
+    // MARK: - Actions
+    func didPressReportIssue() {
+        delegate?.helpViewControllerDidPressReportIssue(self)
+    }
+}
+
+extension HelpViewController {
+    static func instantiate(delegate: HelpViewControllerDelegate) -> Self {
+        let vc = Self()
+        vc.delegate = delegate
+        
+        return vc
+    }
+}
--- Modulite/Screens/SettingsTab/Help/ViewModel/HelpViewModel.swift
+++ Modulite/Screens/SettingsTab/Help/ViewModel/HelpViewModel.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+struct HelpTopicModel {
+    let title: String
+    let text: String
+    
+    init(titleKey: HelpLocalizedTexts, textKey: HelpLocalizedTexts) {
+        title = .localized(for: titleKey)
+        text = .localized(for: textKey)
+    }
+}
+
+class HelpViewModel {
+    
+    private(set) var helpTopics: [HelpTopicModel] = [
+        .init(
+            titleKey: .helpViewProblemSettingUpWidgetTitle,
+            textKey: .helpViewProblemSettingUpWidgetText
+        ),
+        .init(
+            titleKey: .helpViewProblemWithPurchasesTitle,
+            textKey: .helpViewProblemWithPurchasesText
+        ),
+        .init(
+            titleKey: .helpViewCancelSubscriptionTitle,
+            textKey: .helpViewCancelSubscriptionText
+        ),
+        .init(
+            titleKey: .helpViewEncounteredBugTitle,
+            textKey: .helpViewEncounteredBugText1
+        )
+    ]
+}
