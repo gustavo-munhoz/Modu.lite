f11efdb90ab686f72380badf719590902603f374
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-09 19:47:34 -0300

    Added localized texts for help view

--- Modulite/Screens/SettingsTab/Help/HelpLocalizedTexts.swift
+++ Modulite/Screens/SettingsTab/Help/HelpLocalizedTexts.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+enum HelpLocalizedTexts: LocalizedKeyProtocol {
+    case helpViewProblemSettingUpWidgetTitle
+    case helpViewProblemSettingUpWidgetText
+    
+    case helpViewProblemWithPurchasesTitle
+    case helpViewProblemWithPurchasesText
+    
+    case helpViewCancelSubscriptionTitle
+    case helpViewCancelSubscriptionText
+    
+    case helpViewEncounteredBugTitle
+    case helpViewEncounteredBugText1
+    case helpViewEncounteredBugButton
+    case helpViewEncounteredBugText2
+    
+    case helpViewSendEmailErrorAlertTitle
+    case helpViewSendEmailErrorAlertMessage
+    case helpViewSendEmailSubject
+    case helpViewSendEmailBody
+}
