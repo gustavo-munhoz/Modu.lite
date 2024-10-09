234ac1cdccdb692009e0d01c72743815106f7d5f
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-08 22:13:29 -0300

    Added localization texts for tutorials

--- Modulite/Screens/SettingsTab/TutorialCenter/TutorialCenterLocalizedTexts.swift
+++ Modulite/Screens/SettingsTab/TutorialCenter/TutorialCenterLocalizedTexts.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+// swiftlint:disable:next blanket_disable_command
+// swiftlint:disable identifier_name
+
+enum TutorialCenterLocalizedTexts: LocalizedKeyProtocol {
+    case tutorialCenterModuliteTitle
+    case tutorialCenterHowToCreateWidget
+    case tutorialCenterHowToCreateWidgetVideo
+    case tutorialCenterHowToEditOrDeleteWidget
+    
+    case tutorialCenterWidgetsTitle
+    case tutorialCenterAddWidgetsToHomeScreen
+    case tutorialCenterAddWidgetsToHomeScreenVideo
+    
+    case tutorialCenterWallpapersTitle
+    case tutorialCenterSetWallpapers
+    case tutorialCenterChangeWallpaperVideo
+    
+    case tutorialCenterBlockingSessionsTitle
+    case tutorialCenterHowToCreateBlockingSession
+}
