8db53bfafd664dbee37cadf450319910f9f7b842
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-08 22:12:14 -0300

    Implemented new coordinator logic to handle tutorials

--- Modulite/Coordinators/Tutorial/TutorialCoordinator.swift
+++ Modulite/Coordinators/Tutorial/TutorialCoordinator.swift
@@ -5,4 +5,20 @@
 //  Created by Gustavo Munhoz Correa on 08/10/24.
 //
 
-import Foundation
+import UIKit
+
+class TutorialCoordinator<T: UIViewController & TutorialViewController>: Coordinator {
+    var children: [Coordinator] = []
+    
+    var router: Router
+    
+    init(router: Router) {
+        self.router = router
+    }
+    
+    func present(animated: Bool, onDismiss: (() -> Void)?) {
+        let viewController = T()
+        
+        router.present(viewController, animated: animated, onDismiss: onDismiss)
+    }
+}
