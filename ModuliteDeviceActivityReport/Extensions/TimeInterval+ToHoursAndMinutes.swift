9b92a13892836a3dff60cbfd308ed7b213a65404
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-16 19:35:35 -0300

    Created extension for formatting time intervals

--- ModuliteDeviceActivityReport/Extensions/TimeInterval+ToHoursAndMinutes.swift
+++ ModuliteDeviceActivityReport/Extensions/TimeInterval+ToHoursAndMinutes.swift
@@ -14,4 +14,23 @@ extension TimeInterval {
         let hours = (time / 3600)
         return String(format: "%0.2d:%0.2d", hours, minutes)
     }
+    
+    func formattedWithUnits() -> String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute]
+        formatter.unitsStyle = .short
+        formatter.zeroFormattingBehavior = .dropAll
+        formatter.collapsesLargestUnit = false
+        formatter.maximumUnitCount = 2
+        
+        guard let formattedString = formatter.string(from: self) else {
+            return "0 min"
+        }
+                
+        let components = formattedString.split(separator: ",")
+        let spacedComponents = components.map { $0.trimmingCharacters(in: .whitespaces) }
+            .joined(separator: " ")
+        
+        return spacedComponents
+    }
 }
