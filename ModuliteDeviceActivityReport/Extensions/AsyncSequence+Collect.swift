abc6a54955dfb1bbb2f83c6a6c329e57765164c9
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-15 21:55:08 -0300

    Created files for each extension

--- ModuliteDeviceActivityReport/Extensions/AsyncSequence+Collect.swift
+++ ModuliteDeviceActivityReport/Extensions/AsyncSequence+Collect.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+extension AsyncSequence {
+    func collect() async rethrows -> [Element] {
+        try await reduce(into: [Element]()) { $0.append($1) }
+    }
+}
--- ModuliteDeviceActivityReport/Extensions/TimeInterval+ToHoursAndMinutes.swift
+++ ModuliteDeviceActivityReport/Extensions/TimeInterval+ToHoursAndMinutes.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+extension TimeInterval {
+    func toHoursAndMinutes() -> String {
+        let time = NSInteger(self)
+        let minutes = (time / 60) % 60
+        let hours = (time / 3600)
+        return String(format: "%0.2d:%0.2d", hours, minutes)
+    }
+}
