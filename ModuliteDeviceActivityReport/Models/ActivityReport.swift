ab2a1b6ed494b360314ca933a4d6b8e8ca994ef2
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-16 19:35:55 -0300

    Refactored model to have multiple days of reports

--- ModuliteDeviceActivityReport/Models/ActivityReport.swift
+++ ModuliteDeviceActivityReport/Models/ActivityReport.swift
@@ -8,6 +8,17 @@
 import Foundation
 
 struct ActivityReport {
-    let totalDuration: TimeInterval
-    let apps: [AppDeviceActivity]
+    let dailyUsage: [Date: TimeInterval]
+    let averageTimeLastWeek: TimeInterval
+    let appUsage: [Date: [AppDeviceActivity]]
+    
+    func formattedTime(for date: Date) -> String {
+        let calendar = Calendar.current
+        let startOfDay = calendar.startOfDay(for: date)
+        return dailyUsage[startOfDay]?.formattedWithUnits() ?? "-"
+    }
+    
+    var formattedAverageTimeLastWeek: String {
+        averageTimeLastWeek.formattedWithUnits()
+    }
 }
