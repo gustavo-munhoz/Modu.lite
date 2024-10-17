3ccfe2863e019b3bdff3d1653af85564e33f659c
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-17 16:26:22 -0300

    Implemented `AppUsageItem` for showing app stats

--- ModuliteDeviceActivityReport/Views/Components/AppUsageItem.swift
+++ ModuliteDeviceActivityReport/Views/Components/AppUsageItem.swift
@@ -5,4 +5,38 @@
 //  Created by Gustavo Munhoz Correa on 17/10/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct AppUsageItem: View {
+    var app: AppDeviceActivity
+    var usagePercentage: Double
+
+    var body: some View {
+        HStack(spacing: 20) {
+            if let token = app.token {
+                Label(token)
+                    .labelStyle(.iconOnly)
+                    .scaleEffect(1.75)
+            }
+            
+            VStack(alignment: .leading, spacing: 1) {
+                Text(app.displayName)
+                    .font(.headline)
+                    .fontWeight(.semibold)
+                 
+                HStack(spacing: 16) {
+                    GeometryReader { geometry in
+                        RoundedRectangle(cornerRadius: 8)
+                            .frame(width: CGFloat(usagePercentage) * geometry.size.width)
+                            .foregroundColor(.carrotOrange)
+                    }
+                    .frame(height: 12)
+                    
+                    Text(app.duration.formattedAsMinutes())
+                        .font(.callout.weight(.semibold))
+                        .foregroundStyle(.gray)
+                }
+            }
+        }
+    }
+}
