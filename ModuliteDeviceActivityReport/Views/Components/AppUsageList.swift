395caaec0f40e6ce001910fc24d5291994e72836
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-17 16:27:18 -0300

    Implemented list for displaying app statistics

--- ModuliteDeviceActivityReport/Views/Components/AppUsageList.swift
+++ ModuliteDeviceActivityReport/Views/Components/AppUsageList.swift
@@ -5,4 +5,34 @@
 //  Created by Gustavo Munhoz Correa on 17/10/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct AppUsageList: View {
+    var apps: [AppDeviceActivity]
+    
+    var body: some View {
+        let totalDuration = apps.totalDuration
+        
+        ForEach(apps) { app in
+            let rawPercentage = totalDuration > 0 ? (app.duration / totalDuration) : 0
+            let scaledPercentage = scaleLogarithmic(rawPercentage)
+            
+            AppUsageItem(app: app, usagePercentage: scaledPercentage)
+                .padding(.bottom, 20)
+        }
+        .padding(.horizontal, 24)
+    }
+    
+    private func scaleLogarithmic(_ percentage: Double) -> Double {
+        let adjustedValue = max(percentage, 0.01)
+        let logValue = (log(adjustedValue + 1.015) / log(1.5)) * 1.05
+        
+        return min(logValue, 1.0)
+    }
+}
+
+extension Array where Element == AppDeviceActivity {
+    var totalDuration: TimeInterval {
+        reduce(0) { $0 + $1.duration }
+    }
+}
