b3ec794dc758c8851eb18d372179859552eb38ca
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-17 15:43:46 -0300

    Created files for components

---
+++ ModuliteDeviceActivityReport/Views/Components/AppUsageItem.swift
@@ -0,0 +1,8 @@
+//
+//  AppUsageItem.swift
+//  ModuliteDeviceActivityReport
+//
+//  Created by Gustavo Munhoz Correa on 17/10/24.
+//
+
+import Foundation
---
+++ ModuliteDeviceActivityReport/Views/Components/AppUsageList.swift
@@ -0,0 +1,8 @@
+//
+//  AppUsageList.swift
+//  ModuliteDeviceActivityReport
+//
+//  Created by Gustavo Munhoz Correa on 17/10/24.
+//
+
+import Foundation
