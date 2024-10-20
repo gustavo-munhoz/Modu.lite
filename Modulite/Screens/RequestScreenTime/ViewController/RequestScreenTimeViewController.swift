983b44834f5a591a97f0904d2ceb2ba4e8a163a0
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-19 23:43:56 -0300

    Created `RequestScreenTimeView`

--- Modulite/Screens/RequestScreenTime/ViewController/RequestScreenTimeViewController.swift
+++ Modulite/Screens/RequestScreenTime/ViewController/RequestScreenTimeViewController.swift
@@ -7,20 +7,50 @@
 
 import UIKit
 
+protocol RequestScreenTimeViewControllerDelegate: AnyObject {
+    func requestScreenTimeDidPressConnect(_ viewController: RequestScreenTimeViewController)
+    func requestScreenTimeDidPressDismiss(_ viewController: RequestScreenTimeViewController)
+}
+
+extension RequestScreenTimeViewController {
+    static func instantiate(delegate: RequestScreenTimeViewControllerDelegate) -> Self {
+        let vc = Self()
+        vc.delegate = delegate
+        
+        return vc
+    }
+}
+
 class RequestScreenTimeViewController: UIViewController {
     
     // MARK: - Properties
-    private let <#viewName#> = <#UIView#>
+    private let requestView = RequestScreenTimeView()
+    
+    weak var delegate: RequestScreenTimeViewControllerDelegate?
     
     // MARK: - Lifecycle
     override func loadView() {
-        view = <#viewName#>
+        super.loadView()
+        view = requestView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupViewActions()
     }
     
     // MARK: - Setup Methods
+    
+    private func setupViewActions() {
+        requestView.onConnectButtonPress = handleConnectPress
+        requestView.onDismissButtonPress = handleDismissPress
+    }
+    
+    private func handleConnectPress() {
+        delegate?.requestScreenTimeDidPressConnect(self)
+    }
+    
+    private func handleDismissPress() {
+        delegate?.requestScreenTimeDidPressDismiss(self)
+    }
 }
--- Modulite/Screens/Usage/View/RequestScreenTimeViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  RequestScreenTimeViewController.swift
-//  Modulite
-//
-//  Created by Gustavo Munhoz Correa on 18/10/24.
-//
-
-import Foundation
