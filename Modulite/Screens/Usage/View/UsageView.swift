18e3a06264033afb5fbf7fe75ba261c087f67443
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-21 17:31:41 -0300

    Implemented SwiftUI `UsageView` and view model, removing logic from vc

--- Modulite/Screens/Usage/View/UsageView.swift
+++ Modulite/Screens/Usage/View/UsageView.swift
@@ -5,4 +5,30 @@
 //  Created by Gustavo Munhoz Correa on 21/10/24.
 //
 
-import Foundation
+import DeviceActivity
+import SwiftUI
+
+struct UsageView: View {
+    
+    @StateObject private var viewModel = UsageViewModel()
+    
+    var body: some View {
+        Group {
+            if viewModel.isAuthorized {
+                viewModel.createDeviceActivityReport()
+                
+            } else {
+                NavigationStack {
+                    EmptyView()
+                }
+                .navigationTitle("My screen time")
+                .navigationBarTitleDisplayMode(.large)
+                .background(.whiteTurnip)
+            }
+        }
+        .onAppear {
+            viewModel.performAuth()
+        }
+        .background(Color.whiteTurnip.ignoresSafeArea())
+    }
+}
--- Modulite/Screens/Usage/ViewController/UsageViewController.swift
+++ Modulite/Screens/Usage/ViewController/UsageViewController.swift
@@ -2,41 +2,15 @@ import UIKit
 import DeviceActivity
 import SwiftUI
 
-protocol UsageViewControllerDelegate: AnyObject {
-    func usageViewControllerShouldRequestAuth(
-        _ viewController: UsageViewController,
-        onCompletion: @escaping (Result<Void, Error>) -> Void
-    )
-}
-
-extension UsageViewController {
-    static func instantiate(
-        with delegate: UsageViewControllerDelegate
-    ) -> UsageViewController {
-        let vc = UsageViewController()
-        vc.delegate = delegate
-        
-        return vc
-    }
-}
-
-class UsageViewController: UIHostingController<DeviceActivityReport> {
+class UsageViewController: UIHostingController<UsageView> {
     
     // MARK: - Properties
     
     private let reportIdentifier = "TotalActivity"
-    private var viewModel = UsageViewModel()
-    
-    weak var delegate: UsageViewControllerDelegate?
 
     // MARK: - Initializers
     init() {
-        super.init(
-            rootView: DeviceActivityReport(
-                DeviceActivityReport.Context(reportIdentifier),
-                filter: DeviceActivityFilter()
-            )
-        )
+        super.init(rootView: UsageView())
     }
 
     @MainActor @objc required dynamic init?(coder aDecoder: NSCoder) {
@@ -51,94 +25,10 @@ class UsageViewController: UIHostingController<DeviceActivityReport> {
         view.backgroundColor = .whiteTurnip
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        guard UserPreference<ScreenTime>.shared.bool(for: .hasAuthorizedBefore) else {
-            delegate?.usageViewControllerShouldRequestAuth(self) { [weak self] result in
-                switch result {
-                case .success:
-                    self?.fetchAndDisplayActivityReport()
-                case .failure:
-                    break
-                }
-            }
-            return
-        }
-        
-        fetchAndDisplayActivityReport()
-    }
-    
     // MARK: - Setup Methods
 
     private func setupNavigationBar() {
-        navigationItem.title = .localized(for: .usageViewControllerNavigationTitle)
-        navigationController?.navigationBar.prefersLargeTitles = true
-    }
-    
-    private func updateNavigationBarUponLoad() {
-        safeAreaRegions = SafeAreaRegions()
         navigationController?.setNavigationBarHidden(true, animated: false)
-    }
-
-    // MARK: - Data Fetching
-
-    private func fetchAndDisplayActivityReport() {
-        Task {
-            do {
-                try await viewModel.requestAuthorization()
-                let reportView = await createDeviceActivityReport()
-                await MainActor.run {
-                    self.rootView = reportView
-                    
-                    self.updateNavigationBarUponLoad()
-                }
-                
-            } catch {
-                await handleAuthorizationError(error)
-            }
-        }
-    }
-
-    private func createDeviceActivityReport() async -> DeviceActivityReport {
-        let calendar = Calendar.current
-        let now = Date()
-        
-        guard let startOf7DaysAgo = calendar.date(
-            byAdding: .day,
-            value: -7,
-            to: calendar.startOfDay(for: now)
-        ) else {
-            fatalError("Failed to calculate start date for 7 days ago.")
-        }
-        
-        let filter = DeviceActivityFilter(
-            segment: .daily(
-                during: DateInterval(start: startOf7DaysAgo, end: now)
-            )
-        )
-        
-        let context = DeviceActivityReport.Context(reportIdentifier)
-        let reportView = DeviceActivityReport(context, filter: filter)
-        return reportView
-    }
-
-    // MARK: - Error Handling
-
-    private func handleAuthorizationError(_ error: Error) async {
-        await MainActor.run {
-            let alert = UIAlertController(
-                title: NSLocalizedString("Authorization Error", comment: ""),
-                message: error.localizedDescription,
-                preferredStyle: .alert
-            )
-            alert.addAction(
-                UIAlertAction(
-                    title: .localized(for: .ok).uppercased(),
-                    style: .default
-                )
-            )
-            present(alert, animated: true)
-        }
+        safeAreaRegions = SafeAreaRegions()
     }
 }
