eca4e7e15abe74f2f4e0c87edadcbec223288f04
Andre <andre_fabriciow@hotmail.com>
2024-10-22 14:47:11 -0300

    MonitorExtension is commented out, project modified due to additions and name changes

--- ModuliteDeviceActivityMonitor/DeviceActivityMonitorExtension.swift
+++ ModuliteDeviceActivityMonitor/DeviceActivityMonitorExtension.swift
@@ -14,62 +14,62 @@ import ManagedSettings
 
 class DeviceActivityMonitorExtension: DeviceActivityMonitor {
     
-    func showLocalNotification(
-        activityName: DeviceActivityName,
-        title: String,
-        desc: String
-    ) {
-        let model = AppBlockManager(activityName: activityName)
-        let countSelectedAppToken =  model.countSelectedApp()
-        let countSelectedCategoryToken =  model.countSelectedAppCategory()
-        
-        let content = UNMutableNotificationContent()
-        content.title = title
-        content.body = "Selected app: "+String(countSelectedAppToken)+" category: "+String(countSelectedCategoryToken)
-        content.sound = .default
-        
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
-        let request = UNNotificationRequest(identifier: "localNotification", content: content, trigger: trigger)
-        
-        UNUserNotificationCenter.current().add(request) { error in
-            if let error = error {
-                print("Failed to show notification: \(error.localizedDescription)")
-            }
-        }
-        
-        let center = UNUserNotificationCenter.current()
-        center.add(request) { (error) in
-            if let error = error {
-                print("Failed to add notification: \(error)")
-            } else {
-                print("Success add notification")
-            }
-        }
-    }
-    
-    override func intervalDidStart(for activity: DeviceActivityName) {
-        super.intervalDidStart(for: activity)
-        let model = AppBlockManager(activityName: activity)
-        model.startBlock()
-        showLocalNotification(
-            activityName: activity,
-            title: "My Start Restrict App",
-            desc: "Restriction App started successfully"
-        )
-        
-        let socialStore = ManagedSettingsStore(named: ManagedSettingsStore.Name(activity.rawValue))
-        socialStore.clearAllSettings()
-        model.startBlock()
-    }
-    
-    override func intervalDidEnd(for activity: DeviceActivityName) {
-        super.intervalDidEnd(for: activity)
-        let model = AppBlockManager(activityName: activity)
-        model.stopAppRestrictions()
-        showLocalNotification(
-            activityName: activity,
-            title: "My Restriction Stopped",
-            desc: "Restriction App is stopped"
-        )
-    }
+//    func showLocalNotification(
+//        activityName: DeviceActivityName,
+//        title: String,
+//        desc: String
+//    ) {
+//        let model = AppBlockManager(activityName: activityName)
+//        let countSelectedAppToken =  model.countSelectedApp()
+//        let countSelectedCategoryToken =  model.countSelectedAppCategory()
+//        
+//        let content = UNMutableNotificationContent()
+//        content.title = title
+//        content.body = "Selected app: "+String(countSelectedAppToken)+" category: "+String(countSelectedCategoryToken)
+//        content.sound = .default
+//        
+//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+//        let request = UNNotificationRequest(identifier: "localNotification", content: content, trigger: trigger)
+//        
+//        UNUserNotificationCenter.current().add(request) { error in
+//            if let error = error {
+//                print("Failed to show notification: \(error.localizedDescription)")
+//            }
+//        }
+//        
+//        let center = UNUserNotificationCenter.current()
+//        center.add(request) { (error) in
+//            if let error = error {
+//                print("Failed to add notification: \(error)")
+//            } else {
+//                print("Success add notification")
+//            }
+//        }
+//    }
+//    
+//    override func intervalDidStart(for activity: DeviceActivityName) {
+//        super.intervalDidStart(for: activity)
+//        let model = AppBlockManager(activityName: activity)
+//        model.startBlock()
+//        showLocalNotification(
+//            activityName: activity,
+//            title: "My Start Restrict App",
+//            desc: "Restriction App started successfully"
+//        )
+//        
+//        let socialStore = ManagedSettingsStore(named: ManagedSettingsStore.Name(activity.rawValue))
+//        socialStore.clearAllSettings()
+//        model.startBlock()
+//    }
+//    
+//    override func intervalDidEnd(for activity: DeviceActivityName) {
+//        super.intervalDidEnd(for: activity)
+//        let model = AppBlockManager(activityName: activity)
+//        model.stopAppRestrictions()
+//        showLocalNotification(
+//            activityName: activity,
+//            title: "My Restriction Stopped",
+//            desc: "Restriction App is stopped"
+//        )
+//    }
 }
