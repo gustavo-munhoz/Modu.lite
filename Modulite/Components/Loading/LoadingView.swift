ba35da957e8bc0f5bc63924044da64bc5d6d08b7
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-21 21:24:02 -0300

    Created component for loading

--- Modulite/Components/Loading/LoadingView.swift
+++ Modulite/Components/Loading/LoadingView.swift
@@ -5,4 +5,36 @@
 //  Created by Gustavo Munhoz Correa on 21/10/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct LoadingView: View {
+    var imageSize: CGSize = .init(width: 100, height: 100)
+    var hasText: Bool = false
+    
+    @State private var currentImageIndex = 1
+    private let totalImages = 8
+    private let timer = Timer.publish(every: 0.15, on: .main, in: .common).autoconnect()
+
+    var body: some View {
+        VStack {
+            Image("Loading\(currentImageIndex)")
+                .resizable()
+                .scaledToFit()
+                .frame(width: imageSize.width, height: imageSize.height)
+
+            if hasText {
+                Text("Loading...")
+                    .font(.headline)
+                    .padding(.top, 10)
+                    .foregroundStyle(.carrotOrange)
+                    .pulseEffect()
+                    
+            }
+        }
+        .onReceive(timer) { _ in
+            withAnimation {
+                currentImageIndex = currentImageIndex % totalImages + 1
+            }
+        }
+    }
+}
