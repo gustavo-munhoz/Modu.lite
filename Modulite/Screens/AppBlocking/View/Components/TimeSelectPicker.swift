a5672d50b19cb7f98748842969b8c71548305cc7
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-23 16:00:31 -0300

    Created customized date picker

--- Modulite/Screens/AppBlocking/View/Components/TimeSelectPicker.swift
+++ Modulite/Screens/AppBlocking/View/Components/TimeSelectPicker.swift
@@ -5,4 +5,34 @@
 //  Created by Gustavo Munhoz Correa on 23/10/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct TimeSelectPicker: View {
+    var date: Binding<Date>
+    
+    var body: some View {
+        VStack {
+            Text(date.wrappedValue.formatted(.dateTime.hour().minute()))
+                .fontWeight(.semibold)
+                .foregroundStyle(.figBlue)
+        }
+        .frame(minWidth: 55)
+        .padding(.vertical, 6)
+        .padding(.horizontal, 12)
+        .background(
+            RoundedRectangle(cornerRadius: 6)
+                .fill(.potatoYellow)
+                .opacity(0.75)
+        )
+        .overlay {
+            DatePicker(
+                "",
+                selection: date,
+                displayedComponents: .hourAndMinute
+            )
+            .colorMultiply(.clear)
+            .datePickerStyle(DefaultDatePickerStyle())
+            .labelsHidden()
+        }
+    }
+}
