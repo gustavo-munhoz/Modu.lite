86ce40b2b678aace9dc227d53fdc286962c8289c
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-23 16:01:32 -0300

    Created button to selected between scheduled and always on

--- Modulite/Screens/AppBlocking/View/Components/BlockingTypeSelectButton.swift
+++ Modulite/Screens/AppBlocking/View/Components/BlockingTypeSelectButton.swift
@@ -5,4 +5,27 @@
 //  Created by Gustavo Munhoz Correa on 23/10/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct BlockingTypeSelectButton: View {
+    var title: String
+    @Binding var isSelected: Bool
+    
+    var body: some View {
+        Button(action: {
+            isSelected = true
+        }, label: {
+            Text(title)
+                .font(.title3.bold())
+                .foregroundStyle(isSelected ? .white : .black)
+                .padding(.vertical, 12)
+                .frame(maxWidth: .infinity)
+                .background(isSelected ? Color.carrotOrange : Color.clear)
+                .cornerRadius(8)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.carrotOrange, lineWidth: 2)
+                )
+        })
+    }
+}
