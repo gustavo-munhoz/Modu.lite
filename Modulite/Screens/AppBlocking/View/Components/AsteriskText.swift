5a89c0233fc7f0126134459761bc8913cd9ce803
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-23 16:00:48 -0300

    Created italic and bold Text with asterisk before it

--- Modulite/Screens/AppBlocking/View/Components/AsteriskText.swift
+++ Modulite/Screens/AppBlocking/View/Components/AsteriskText.swift
@@ -5,4 +5,26 @@
 //  Created by Gustavo Munhoz Correa on 23/10/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct AsteriskText: View {
+    var text: String = ""
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 4) {
+            Image(systemName: "asterisk")
+                .resizable()
+                .fontWeight(.heavy)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 21, height: 21)
+                .foregroundColor(.lemonYellow)
+                        
+            Text(text)
+                .font(.system(.title2, design: .default).bold().italic())
+                .lineSpacing(5)
+                .multilineTextAlignment(.leading)
+                .padding(.leading, 4)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
