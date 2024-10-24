186a91bb23a4538ab657193f8d1ab4bb34c109fe
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-24 14:13:26 -0300

    Implemented cells for `sessionsCollectionView`

--- Modulite/Screens/AppBlocking/View/AppBlockingSessionHeader.swift
+++ Modulite/Screens/AppBlocking/View/AppBlockingSessionHeader.swift
@@ -5,4 +5,42 @@
 //  Created by Gustavo Munhoz Correa on 24/10/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class AppBlockingSessionHeader: HomeHeaderReusableCell {
+    
+    private let separator = SeparatorView()
+    
+    func setup(
+        title: String,
+        hasButton: Bool,
+        buttonAction: @escaping () -> Void = {},
+        countValues: (current: Int, max: Int)? = nil
+    ) {
+        setup(
+            title: title,
+            buttonImage: hasButton ? UIImage(systemName: "plus.circle") : nil,
+            buttonAction: buttonAction,
+            countValues: countValues
+        )
+        
+        // FIXME: Constraints are having conflicts
+        
+        addSubview(separator)
+        separator.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.left.right.equalToSuperview()
+        }
+                
+        titleLabel.snp.remakeConstraints { make in
+            make.top.equalTo(separator.snp.bottom).offset(16)
+            make.left.equalToSuperview()
+        }
+        
+        actionButton.snp.remakeConstraints { make in
+            make.top.equalTo(separator.snp.bottom).offset(16)
+            make.right.equalToSuperview()
+        }
+    }
+}
