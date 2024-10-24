fb83c674a1b81fe537bbd4e38d93e813d77695b7
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-24 14:14:12 -0300

    Created version of view model for tests

--- Modulite/Screens/AppBlocking/ViewModel/AppBlockingViewModel.swift
+++ Modulite/Screens/AppBlocking/ViewModel/AppBlockingViewModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+class AppBlockingViewModel {
+    
+    @Published var activeSessions: [AppBlockingSession] = [
+        .init(sessionName: "My session")
+    ]
+    
+    @Published var inactiveSessions: [AppBlockingSession] = [
+        .init(sessionName: "Other session"),
+        .init(sessionName: "Other session"),
+        .init(sessionName: "Other session"),
+        .init(sessionName: "Other session")
+    ]
+}
