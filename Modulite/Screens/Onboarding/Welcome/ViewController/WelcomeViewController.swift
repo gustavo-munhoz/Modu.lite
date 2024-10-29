aa1a2d9c92cf4ff72861f6eb92fca8ac16e52b63
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-29 19:19:35 -0300

    Created `WelcomeView`

--- Modulite/Screens/Onboarding/Welcome/ViewController/WelcomeViewController.swift
+++ Modulite/Screens/Onboarding/Welcome/ViewController/WelcomeViewController.swift
@@ -5,4 +5,25 @@
 //  Created by Gustavo Munhoz Correa on 29/10/24.
 //
 
-import Foundation
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    // MARK: - Properties
+    private let welcomeView = WelcomeView()
+    
+    // MARK: - Lifecycle
+    override func loadView() {
+        view = welcomeView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViewActions()
+    }
+    
+    // MARK: - Setup Methods
+    private func setupViewActions() {
+        
+    }
+}
