40da354aabaccd30b2b664af754d64bbf2c418eb
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-29 19:20:43 -0300

    Created `hasCompletedOnboarding` preference

--- Modulite/Singleton/UserPreferences/OnboardingPreferences.swift
+++ Modulite/Singleton/UserPreferences/OnboardingPreferences.swift
@@ -5,4 +5,10 @@
 //  Created by Gustavo Munhoz Correa on 29/10/24.
 //
 
-import Foundation
+enum Onboarding: String, UserPreferenceKey {
+    var key: String {
+        self.rawValue
+    }
+    
+    case hasCompletedOnboarding
+}
