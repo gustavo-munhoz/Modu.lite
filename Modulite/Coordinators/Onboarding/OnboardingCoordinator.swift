f17b5155cdeab17d9398a62d428fd0362d1dc82e
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-29 19:22:39 -0300

    Created coordinator for presenting onboarding

--- Modulite/Coordinators/Onboarding/OnboardingCoordinator.swift
+++ Modulite/Coordinators/Onboarding/OnboardingCoordinator.swift
@@ -5,4 +5,27 @@
 //  Created by Gustavo Munhoz Correa on 29/10/24.
 //
 
-import Foundation
+import UIKit
+
+class OnboardingCoordinator: Coordinator {
+    /// Child coordinators managed by this coordinator.
+    var children: [Coordinator] = []
+    
+    /// The router used to present view controllers.
+    let router: Router
+    
+    /// Initializes a new instance of `BlockAppsCoordinator` with a router.
+    /// - Parameter router: The router that will handle presentation and dismissal of view controllers.
+    init(router: Router) {
+        self.router = router
+    }
+    
+    /// Presents the block apps view controller using the associated router.
+    /// - Parameters:
+    ///   - animated: Determines if the presentation should be animated.
+    ///   - onDismiss: Optional closure to execute when the block apps view controller is dismissed.
+    func present(animated: Bool, onDismiss: (() -> Void)?) {
+        let vc = WelcomeViewController()
+        router.present(vc, animated: animated, onDismiss: onDismiss)
+    }
+}
