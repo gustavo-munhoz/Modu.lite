7f4eb9e1d27464ea9a6842bb3708316281883d90
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-29 19:18:54 -0300

    Created new files for onboarding

---
+++ Modulite/Screens/Onboarding/OnboardingLocalizedTexts.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingLocalizedTexts.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 29/10/24.
+//
+
+import Foundation
---
+++ Modulite/Screens/Onboarding/Welcome/ViewController/WelcomeViewController.swift
@@ -0,0 +1,8 @@
+//
+//  WelcomeViewController.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 29/10/24.
+//
+
+import Foundation
