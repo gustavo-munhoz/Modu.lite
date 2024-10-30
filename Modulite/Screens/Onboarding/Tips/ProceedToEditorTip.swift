56f14bc3d576c81a735e24342e8731d00c9e4bb9
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-30 20:53:13 -0300

    Created tip to proceed to editor

--- Modulite/Screens/Onboarding/Tips/ProceedToEditorTip.swift
+++ Modulite/Screens/Onboarding/Tips/ProceedToEditorTip.swift
@@ -5,4 +5,19 @@
 //  Created by Gustavo Munhoz Correa on 30/10/24.
 //
 
-import Foundation
+import SwiftUI
+import TipKit
+
+struct ProceedToEditorTip: Tip {
+    var title: Text {
+        Text(
+            String.localized(for: OnboardingLocalizedTexts.onboardingProceedToEditorTitle)
+        )
+    }
+    
+    var rules: [Rule] {
+        #Rule(WidgetSetupViewController.didSelectApps) {
+            $0.donations.count > 0
+        }
+    }
+}
