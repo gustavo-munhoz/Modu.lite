28df7ea56a0c37a239a8268bfbfeee40571db65b
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-30 18:12:32 -0300

    Implemented tip for selecting apps

--- Modulite/Screens/Onboarding/Tips/SelectAppsTip.swift
+++ Modulite/Screens/Onboarding/Tips/SelectAppsTip.swift
@@ -5,4 +5,25 @@
 //  Created by Gustavo Munhoz Correa on 30/10/24.
 //
 
-import Foundation
+import SwiftUI
+import TipKit
+
+struct SelectAppsTip: Tip {
+    var title: Text {
+        Text(
+            String.localized(for: OnboardingLocalizedTexts.onboardingSelectAppsTitle)
+        )
+    }
+    
+    var message: Text? {
+        Text(
+            String.localized(for: OnboardingLocalizedTexts.onboardingSelectAppsMessage)
+        )
+    }
+    
+    var rules: [Rule] {
+        #Rule(WidgetSetupViewController.didSelectWidgetStyle) {
+            $0.donations.count > 0
+        }
+    }
+}
