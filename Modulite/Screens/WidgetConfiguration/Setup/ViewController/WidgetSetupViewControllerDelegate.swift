e999599b8182cc5f45c7570af168122d2f481cf5
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-30 20:50:05 -0300

    Moved delegate implementation to other file

--- Modulite/Screens/WidgetConfiguration/Setup/ViewController/WidgetSetupViewControllerDelegate.swift
+++ Modulite/Screens/WidgetConfiguration/Setup/ViewController/WidgetSetupViewControllerDelegate.swift
@@ -5,4 +5,29 @@
 //  Created by Gustavo Munhoz Correa on 30/10/24.
 //
 
-import Foundation
+import UIKit
+
+protocol WidgetSetupViewControllerDelegate: AnyObject {
+    func getPlaceholderName() -> String
+    
+    func widgetSetupViewControllerDidPressNext(widgetName: String)
+    
+    func widgetSetupViewControllerDidTapSearchApps(
+        _ parentController: WidgetSetupViewController
+    )
+    
+    func widgetSetupViewControllerDidDeselectApp(
+        _ controller: WidgetSetupViewController,
+        app: AppInfo
+    )
+    
+    func widgetSetupViewControllerDidSelectWidgetStyle(
+        _ controller: WidgetSetupViewController,
+        style: WidgetStyle
+    )
+    
+    func widgetSetupViewControllerDidPressBack(
+        _ viewController: WidgetSetupViewController,
+        didMakeChanges: Bool
+    )
+}
