76bd2544f931ae4013784312044e72f926525186
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-30 22:45:28 -0300

    Created tip for editing modules

--- Modulite/Screens/Onboarding/Tips/EditModuleTip.swift
+++ Modulite/Screens/Onboarding/Tips/EditModuleTip.swift
@@ -5,4 +5,19 @@
 //  Created by Gustavo Munhoz Correa on 30/10/24.
 //
 
-import Foundation
+import SwiftUI
+import TipKit
+
+struct EditModuleTip: Tip {
+    var title: Text {
+        Text(
+            String.localized(for: OnboardingLocalizedTexts.onboardingEditModulesTitle)
+        )
+    }
+    
+    var rules: [Rule] {
+        #Rule(WidgetEditorViewController.didDragModule) {
+            $0.donations.count > 0
+        }
+    }
+}
