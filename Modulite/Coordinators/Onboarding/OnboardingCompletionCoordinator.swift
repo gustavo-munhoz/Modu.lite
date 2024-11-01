a4c6459e79f1cb8bbd6a385c3e17adff83be8b6e
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-11-01 18:09:28 -0300

    Implemented onboarding completion coordinator

--- Modulite/Coordinators/Onboarding/OnboardingCompletionCoordinator.swift
+++ Modulite/Coordinators/Onboarding/OnboardingCompletionCoordinator.swift
@@ -5,4 +5,49 @@
 //  Created by Gustavo Munhoz Correa on 31/10/24.
 //
 
-import Foundation
+import UIKit
+
+class OnboardingCompletionCoordinator: Coordinator {
+    /// Child coordinators managed by this coordinator.
+    var children: [Coordinator] = []
+    
+    /// The router used to present view controllers.
+    let router: Router
+    
+    /// Initializes a new instance of `BlockAppsCoordinator` with a router.
+    /// - Parameter router: The router that will handle presentation and dismissal of view controllers.
+    init(router: Router) {
+        self.router = router
+    }
+    
+    /// Presents the block apps view controller using the associated router.
+    /// - Parameters:
+    ///   - animated: Determines if the presentation should be animated.
+    ///   - onDismiss: Optional closure to execute when the block apps view controller is dismissed.
+    func present(animated: Bool, onDismiss: (() -> Void)?) {
+        let vc = OnboardingTutorialsViewController()
+        vc.delegate = self
+        
+        router.present(vc, animated: animated, onDismiss: onDismiss)
+    }
+}
+
+extension OnboardingCompletionCoordinator: OnboardingTutorialsControllerDelegate {
+    func onboardingTutorialsViewController(
+        _ viewController: OnboardingTutorialsViewController,
+        didPressPresent tutorial: OnboardingTutorialType
+    ) {
+        let router = ModalNavigationRouter(parentViewController: viewController)
+        router.setHasSaveButton(false)
+        
+        switch tutorial {
+        case .wallpaper:
+            let coordinator = TutorialCoordinator<TutorialWallpaperViewController>(router: router)
+            presentChild(coordinator, animated: true)
+            
+        case .widget:
+            let coordinator = TutorialCoordinator<TutorialAddWidgetViewController>(router: router)
+            presentChild(coordinator, animated: true)
+        }
+    }
+}
