288ffe2eb112d670ade5af3d4af178843e5129b7
André Wozniack <84557148+AndreWozniack@users.noreply.github.com>
2024-11-04 23:44:34 -0300

    Created UnlockBadge component

--- Modulite/Components/Skins/SkinUnlocklBadge.swift
+++ Modulite/Components/Skins/SkinUnlocklBadge.swift
@@ -6,8 +6,75 @@
 //
 
 import UIKit
+import SnapKit
 
-class SkinUnlockBadge: GradientLabelView {
+class SkinUnlockBadge: UIView {
+    
+    // MARK: - Properties
+    private(set) lazy var unlockLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 8)
+        label.textColor = .black
+        label.text = .localized(for: BadgeLocalizedTexts.skinUnlockFor)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private(set) lazy var priceLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 15)
+        label.textColor = .black
+        label.text = .localized(for: BadgeLocalizedTexts.skin099)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let unlockBadge = SkinUnlockSmallBadge()
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubviews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup methods
+    private func addSubviews() {
+        addSubview(unlockBadge)
+        addSubview(unlockLabel)
+        addSubview(priceLabel)
+
+    }
+    
+    private func setupConstraints() {
+        unlockBadge.snp.makeConstraints { make in
+            make.centerY.centerX.equalToSuperview()
+            make.width.equalTo(84)
+            make.height.equalTo(40)
+        }
+        
+        snp.makeConstraints { make in
+            make.width.equalTo(84)
+            make.height.equalTo(40)
+        }
+        
+        unlockLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(5)
+            make.left.right.equalToSuperview().offset(14)
+        }
+        
+        priceLabel.snp.makeConstraints { make in
+            make.top.equalTo(unlockLabel.snp.bottom).offset(2)
+            make.centerX.equalToSuperview()
+        }
+    }
+}
+
+class SkinUnlockSmallBadge: GradientLabelView {
     convenience init() {
         self.init(
             gradient: .ambrosia(),
@@ -15,18 +82,6 @@ class SkinUnlockBadge: GradientLabelView {
         )
         
         textAlignment = .center
-        attributedText = NSAttributedString(
-            string: .localized(for: .plus),
-            attributes: [
-                .foregroundColor: UIColor.white,
-                .font: UIFont.systemFont(
-                    ofSize: 16,
-                    weight: .heavy,
-                    width: .expanded
-                )
-            ]
-        )
-        
         layer.cornerRadius = 6.25
         clipsToBounds = true
     }
