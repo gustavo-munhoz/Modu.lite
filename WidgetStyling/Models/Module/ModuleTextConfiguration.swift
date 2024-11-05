cad0cf86612f23f3a6b600d448c17b18af150e8e
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-11-05 17:42:47 -0300

    Created protocols for modules

--- WidgetStyling/Models/Module/ModuleTextConfiguration.swift
+++ WidgetStyling/Models/Module/ModuleTextConfiguration.swift
@@ -5,4 +5,59 @@
 //  Created by Gustavo Munhoz Correa on 05/11/24.
 //
 
-import Foundation
+import UIKit
+
+public class ModuleTextConfiguration {
+    var font: UIFont?
+    var textColor: UIColor?
+    var textAlignment: NSTextAlignment?
+    var shadowColor: UIColor?
+    var shadowOffset: CGSize?
+    var shadowBlurRadius: CGFloat?
+    var letterSpacing: CGFloat?
+    var textCase: String.TextCase?
+    var shouldRemoveSpaces: Bool = false
+    var preffix: String?
+    var suffix: String?
+
+    func font(_ font: UIFont) -> Self {
+        self.font = font
+        return self
+    }
+    
+    func textColor(_ color: UIColor) -> Self {
+        self.textColor = color
+        return self
+    }
+    
+    func shadow(
+        color: UIColor,
+        offset: CGSize = .zero,
+        blurRadius: CGFloat = .zero
+    ) -> Self {
+        self.shadowColor = color
+        self.shadowOffset = offset
+        self.shadowBlurRadius = blurRadius
+        return self
+    }
+    
+    func textCase(_ textCase: String.TextCase) -> Self {
+        self.textCase = textCase
+        return self
+    }
+    
+    func removingSpaces() -> Self {
+        self.shouldRemoveSpaces = true
+        return self
+    }
+    
+    func preffix(_ preffix: String) -> Self {
+        self.preffix = preffix
+        return self
+    }
+    
+    func suffix(_ suffix: String) -> Self {
+        self.suffix = suffix
+        return self
+    }
+}
