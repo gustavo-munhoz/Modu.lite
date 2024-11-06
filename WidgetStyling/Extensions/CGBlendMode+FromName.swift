52cb265111a093678edac8f4252b708d98c9c467
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-11-06 17:48:04 -0300

    Created extension to create blend modes with name

--- WidgetStyling/Extensions/CGBlendMode+FromName.swift
+++ WidgetStyling/Extensions/CGBlendMode+FromName.swift
@@ -5,4 +5,41 @@
 //  Created by Gustavo Munhoz Correa on 06/11/24.
 //
 
-import Foundation
+import CoreGraphics
+
+extension CGBlendMode {
+    static func named(_ name: String) -> CGBlendMode? {
+        let blendModes: [String: CGBlendMode] = [
+            "normal": .normal,
+            "multiply": .multiply,
+            "screen": .screen,
+            "overlay": .overlay,
+            "darken": .darken,
+            "lighten": .lighten,
+            "colorDodge": .colorDodge,
+            "colorBurn": .colorBurn,
+            "softLight": .softLight,
+            "hardLight": .hardLight,
+            "difference": .difference,
+            "exclusion": .exclusion,
+            "hue": .hue,
+            "saturation": .saturation,
+            "color": .color,
+            "luminosity": .luminosity,
+            "clear": .clear,
+            "copy": .copy,
+            "sourceIn": .sourceIn,
+            "sourceOut": .sourceOut,
+            "sourceAtop": .sourceAtop,
+            "destinationOver": .destinationOver,
+            "destinationIn": .destinationIn,
+            "destinationOut": .destinationOut,
+            "destinationAtop": .destinationAtop,
+            "xor": .xor,
+            "plusDarker": .plusDarker,
+            "plusLighter": .plusLighter
+        ]
+        
+        return blendModes[name.lowercased()]
+    }
+}
