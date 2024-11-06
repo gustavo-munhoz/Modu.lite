8bfc4005c76115c353bfb5afd8795a3ce7ef4ec5
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-11-05 22:33:43 -0300

    Implemented basic version of `WidgetModule`

--- WidgetStyling/Models/Widget/WidgetModule.swift
+++ WidgetStyling/Models/Widget/WidgetModule.swift
@@ -5,4 +5,40 @@
 //  Created by Gustavo Munhoz Correa on 05/11/24.
 //
 
-import Foundation
+import UIKit
+
+public class WidgetModule {
+    
+    // MARK: - Properties
+    var isEmpty: Bool { appName == nil }
+    
+    var position: Int
+    var style: ModuleStyle
+    let appName: String?
+    let urlScheme: URL?
+    var color: UIColor
+    
+    // MARK: - Initializers
+    init(
+        style: ModuleStyle,
+        position: Int,
+        appName: String?,
+        urlScheme: URL?,
+        color: UIColor
+    ) {
+        self.style = style
+        self.position = position
+        self.appName = appName
+        self.urlScheme = urlScheme
+        self.color = color
+    }
+}
+
+extension Array where Element: WidgetModule {
+    mutating func replace(at position: Int, with module: Element) {
+        replaceSubrange(
+            position...position,
+            with: [module]
+        )
+    }
+}
