76a9dc6eda73767bee5db5fe2acf915358dfa2b8
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-11-06 18:20:08 -0300

    Created model for `PreviewSet`

---
+++ WidgetStyling/Models/Style/Decodable/PreviewSetData.swift
@@ -0,0 +1,15 @@
+//
+//  PreviewSetData.swift
+//  WidgetStyling
+//
+//  Created by Gustavo Munhoz Correa on 06/11/24.
+//
+
+import Foundation
+
+struct PreviewSetData: Decodable {
+    let widgetPreviewName: String
+    let wallpaperPreview1Name: String
+    let wallpaperPreview2Name: String
+    let wallpaperPreview3Name: String
+}
---
+++ WidgetStyling/Models/Style/PreviewSet.swift
@@ -0,0 +1,36 @@
+//
+//  PreviewSet.swift
+//  WidgetStyling
+//
+//  Created by Gustavo Munhoz Correa on 06/11/24.
+//
+
+import UIKit
+
+public struct PreviewSet {
+    let widgetPreview: UIImage
+    let wallpaperPreview1: UIImage
+    let wallpaperPreview2: UIImage
+    let wallpaperPreview3: UIImage
+    
+    enum PreviewError: Swift.Error {
+        case imageNotFound
+    }
+    
+    static func create(from data: PreviewSetData) throws -> PreviewSet {
+        guard let widgetPreview = UIImage(named: data.widgetPreviewName),
+              let wallpaperPreview1 = UIImage(named: data.wallpaperPreview1Name),
+              let wallpaperPreview2 = UIImage(named: data.wallpaperPreview2Name),
+              let wallpaperPreview3 = UIImage(named: data.wallpaperPreview3Name)
+        else {
+            throw PreviewError.imageNotFound
+        }
+        
+        return PreviewSet(
+            widgetPreview: widgetPreview,
+            wallpaperPreview1: wallpaperPreview1,
+            wallpaperPreview2: wallpaperPreview2,
+            wallpaperPreview3: wallpaperPreview3
+        )
+    }
+}
