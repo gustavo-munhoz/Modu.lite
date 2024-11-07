aa40374db71b8cb158d1e23757a3b81e7b84693f
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-11-07 16:23:17 -0300

    Created methods to help persisting and fetching `WidgetModule`s

--- Modulite/Extensions/WidgetModule+Extensions.swift
+++ Modulite/Extensions/WidgetModule+Extensions.swift
@@ -1,8 +1,42 @@
 //
-//  WidgetModule+extensions.swift
+//  WidgetModule+Extensions.swift
 //  Modulite
 //
 //  Created by Gustavo Munhoz Correa on 07/11/24.
 //
 
-import Foundation
+import UIKit
+import WidgetStyling
+
+extension WidgetModule {
+    func createCompleteImage() -> UIImage {
+        let cell = WidgetModuleCell()
+        cell.setup(with: self)
+        cell.frame = CGRect(
+            origin: .zero,
+            size: CGSize(width: 108, height: 170)
+        )
+        
+        cell.layoutIfNeeded()
+        
+        return cell.asImage()
+    }
+}
+
+extension WidgetModule {
+    convenience init?(persisted: PersistableModuleConfiguration) {
+        let provider = try? WidgetStyleProvider()
+        
+        guard let moduleStyle = provider?.getModuleStyle(
+            by: persisted.selectedStyleKey
+        ) else { return nil }
+        
+        self.init(
+            style: moduleStyle,
+            position: Int(persisted.index),
+            appName: persisted.appName,
+            urlScheme: persisted.urlScheme,
+            color: persisted.selectedColor ?? .clear
+        )
+    }
+}
