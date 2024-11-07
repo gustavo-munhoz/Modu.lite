4b2107131297a4f41cd73222244c4b5459bc79c8
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-11-07 16:18:12 -0300

    Created files in main bundle and removed string extension

---
+++ Modulite/Extensions/WidgetModule+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  WidgetModule+extensions.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 07/11/24.
+//
+
+import Foundation
---
+++ Modulite/Extensions/WidgetSchema+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  WidgetSchema+Extensions.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 07/11/24.
+//
+
+import Foundation
