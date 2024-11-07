f3bfc28289d74c2b624c1908e7a037229dbb992e
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-11-06 22:12:56 -0300

    Added extensions to create objects with strings

--- WidgetStyling/Extensions/NSTextAlignment+FromString.swift
+++ WidgetStyling/Extensions/NSTextAlignment+FromString.swift
@@ -5,4 +5,23 @@
 //  Created by Gustavo Munhoz Correa on 06/11/24.
 //
 
-import Foundation
+import UIKit
+
+extension NSTextAlignment {
+    init?(from string: String) {
+        switch string.lowercased() {
+        case "left":
+            self = .left
+        case "center":
+            self = .center
+        case "right":
+            self = .right
+        case "justified":
+            self = .justified
+        case "natural":
+            self = .natural
+        default:
+            return nil
+        }
+    }
+}
--- WidgetStyling/Extensions/UIFont.Weight+FromString.swift
+++ WidgetStyling/Extensions/UIFont.Weight+FromString.swift
@@ -5,4 +5,35 @@
 //  Created by Gustavo Munhoz Correa on 06/11/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIFont.Weight {
+    static func from(string: String?) -> UIFont.Weight {
+        guard let string = string?.lowercased() else {
+            return .regular
+        }
+        
+        switch string {
+        case "ultralight":
+            return .ultraLight
+        case "thin":
+            return .thin
+        case "light":
+            return .light
+        case "regular":
+            return .regular
+        case "medium":
+            return .medium
+        case "semibold":
+            return .semibold
+        case "bold":
+            return .bold
+        case "heavy":
+            return .heavy
+        case "black":
+            return .black
+        default:
+            return .regular
+        }
+    }
+}
