661ce0cb36eafc4df496ce322e5bf1bfed04c012
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-11-09 18:28:39 -0300

    Renamed `AppInfo` to `PersitentAppData`

--- Modulite/Models/AppInfo/Persistence/PersistentAppData+CoreDataClass.swift
+++ Modulite/Models/AppInfo/Persistence/PersistentAppData+CoreDataClass.swift
@@ -5,4 +5,9 @@
 //  Created by Gustavo Munhoz Correa on 08/11/24.
 //
 
-import Foundation
+import CoreData
+
+@objc(PersistentAppData)
+class PersistentAppData: NSManagedObject {
+    
+}
--- Modulite/Models/AppInfo/Persistence/PersistentAppData+CoreDataProperties.swift
+++ Modulite/Models/AppInfo/Persistence/PersistentAppData+CoreDataProperties.swift
@@ -5,4 +5,57 @@
 //  Created by Gustavo Munhoz Correa on 08/11/24.
 //
 
-import Foundation
+import CoreData
+import WidgetStyling
+
+extension PersistentAppData {
+    @NSManaged var name: String
+    @NSManaged var urlScheme: URL
+    @NSManaged var relevance: UInt16
+}
+
+extension PersistentAppData {
+    static func from(
+        data: AppData,
+        using managedObjectContext: NSManagedObjectContext
+    ) {
+        let app = PersistentAppData(context: managedObjectContext)
+        
+        app.name = data.name
+        app.urlScheme = data.urlScheme
+        app.relevance = UInt16(data.relevance)
+        
+        do {
+            try managedObjectContext.save()
+        } catch {
+            print("Error saving app data: \(error.localizedDescription)")
+        }
+    }
+}
+
+extension PersistentAppData {
+    static func nameSortedFetchRequest() -> NSFetchRequest<PersistentAppData> {
+        guard let request = PersistentAppData.fetchRequest() as? NSFetchRequest<PersistentAppData> else {
+            fatalError("Could not create fetch request for PersistentAppData.")
+        }
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        
+        return request
+    }
+}
+
+extension PersistentAppData {
+    static func prioritySortedFetchRequest() -> NSFetchRequest<PersistentAppData> {
+        guard let request = PersistentAppData.fetchRequest() as? NSFetchRequest<PersistentAppData> else {
+            fatalError("Could not create fetch request for PersistentAppData.")
+        }
+        
+        request.sortDescriptors = [
+            NSSortDescriptor(key: "relevance", ascending: true),
+            NSSortDescriptor(key: "name", ascending: true)
+        ]
+        
+        return request
+    }
+}
