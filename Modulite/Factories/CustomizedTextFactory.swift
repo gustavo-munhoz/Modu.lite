5740f5da9ba833c7a2f9bf420333e58fddc9243a
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-11-12 20:34:18 -0300

    Fixed asterisk sizing

--- Modulite/Factories/CustomizedTextFactory.swift
+++ Modulite/Factories/CustomizedTextFactory.swift
@@ -16,32 +16,36 @@ class CustomizedTextFactory {
         symbolicTraits: UIFontDescriptor.SymbolicTraits = .traitBold.union(.traitItalic),
         paragraphHeadIndent: CGFloat = 28
     ) -> NSAttributedString {
-        let imageAttachment = NSTextAttachment(
-            image: UIImage(systemName: "asterisk")!
-                .withTintColor(.lemonYellow, renderingMode: .alwaysOriginal)
-        )
-
+        let imageAttachment = NSTextAttachment()
+        let image = UIImage(systemName: "asterisk")?
+            .withTintColor(.lemonYellow, renderingMode: .alwaysOriginal)
+            .withConfiguration(UIImage.SymbolConfiguration(weight: .heavy))
+                
+        imageAttachment.image = image
+        imageAttachment.bounds = asteriskRect
+                
         let attachmentString = NSAttributedString(attachment: imageAttachment)
-        let completeText = NSMutableAttributedString("")
-        
+        let completeText = NSMutableAttributedString()
+                
         completeText.append(attachmentString)
-        completeText.append(NSAttributedString("  "))
-        
+        completeText.append(NSAttributedString(string: "  "))
+                
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.headIndent = paragraphHeadIndent
         paragraphStyle.firstLineHeadIndent = 0
-        
+                
         completeText.addAttributes(
             [.font: UIFont(textStyle: textStyle, weight: .heavy)],
             range: NSRange(location: 0, length: 1)
         )
-        
+                
         let titleString = NSAttributedString(string: text, attributes: [
             .font: UIFont(textStyle: textStyle, symbolicTraits: symbolicTraits)!,
             .paragraphStyle: paragraphStyle
         ])
-        
+                
         completeText.append(titleString)
+                
         completeText.addAttributes(
             [.paragraphStyle: paragraphStyle],
             range: NSRange(location: 0, length: completeText.length)
