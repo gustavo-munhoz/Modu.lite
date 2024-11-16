f489cc4d19e7d92c530a5608171ad4f627263309
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-11-15 21:58:27 -0300

    Changed update purchase styles logic

--- WidgetStyling/Providers/WidgetStyleProvider.swift
+++ WidgetStyling/Providers/WidgetStyleProvider.swift
@@ -24,9 +24,9 @@ public class WidgetStyleProvider {
     // MARK: - Methods
     public func setPurchasedStyles(_ styleIds: Set<String>) {
         styles = styles.map {
-            $0.updateIsPurchased(
-                to: styleIds.contains($0.identifier)
-            )
+            if styleIds.contains($0.identifier) {
+                $0.updateIsPurchased(to: true)
+            }
             return $0
         }
     }
