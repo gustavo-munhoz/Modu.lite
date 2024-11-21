f99bb2e55cc1e852a5d8706f53f8595aefbaae4d
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-11-21 17:10:44 -0300

    Added `LoadingViewController` that is displayed while setting up

--- Modulite/Screens/Loading/ViewController/LoadingViewController.swift
+++ Modulite/Screens/Loading/ViewController/LoadingViewController.swift
@@ -5,4 +5,17 @@
 //  Created by Gustavo Munhoz Correa on 21/11/24.
 //
 
-import Foundation
+import UIKit
+
+class LoadingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+        view.backgroundColor = .whiteTurnip
+                
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.center = view.center
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+    }
+}
