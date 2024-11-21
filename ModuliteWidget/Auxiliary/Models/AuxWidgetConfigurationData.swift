d1462bc84688299d1de112339faaedd2889799f8
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-11-21 03:57:58 -0300

    Created files for auxiliary widgets

---
+++ ModuliteWidget/Auxiliary/Models/AuxWidgetConfigurationData.swift
@@ -0,0 +1,8 @@
+//
+//  AuxWidgetConfigurationData.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 21/11/24.
+//
+
+import Foundation
---
+++ ModuliteWidget/Auxiliary/Views/AuxWidgetView.swift
@@ -0,0 +1,8 @@
+//
+//  AuxWidgetView.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 21/11/24.
+//
+
+import Foundation
