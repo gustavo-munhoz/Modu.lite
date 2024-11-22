5f016744dad9892004162edb39d152276f80f19b
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-11-21 23:23:15 -0300

    Implemented major changes view and controller

--- Modulite/Screens/MajorChanges/ViewController/MajorChangesViewController.swift
+++ Modulite/Screens/MajorChanges/ViewController/MajorChangesViewController.swift
@@ -5,4 +5,35 @@
 //  Created by Gustavo Munhoz Correa on 21/11/24.
 //
 
-import Foundation
+import UIKit
+
+protocol MajorChangesViewControllerDelegate: AnyObject {
+    func majorChangesViewControllerDidTapGotIt(
+        _ viewController: MajorChangesViewController
+    )
+}
+
+class MajorChangesViewController: UIViewController {
+    
+    // MARK: - Properties
+    private let majorChangesView = MajorChangesView()
+    
+    weak var delegate: MajorChangesViewControllerDelegate?
+    
+    // MARK: - Lifecycle
+    override func loadView() {
+        super.loadView()
+        view = majorChangesView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        majorChangesView.onGotItButtonPress = didPressGotIt
+    }
+    
+    // MARK: - Actions
+    private func didPressGotIt() {
+        delegate?.majorChangesViewControllerDidTapGotIt(self)
+    }
+}
