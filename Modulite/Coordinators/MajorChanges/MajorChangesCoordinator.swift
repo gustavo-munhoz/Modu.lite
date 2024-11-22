85eab271fd2889e710594f7b4d19fff942d125d6
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-11-21 23:21:41 -0300

    Created new files for major changes sheet

---
+++ Modulite/Coordinators/MajorChanges/MajorChangesCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  MajorChangesCoordinator.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 21/11/24.
+//
+
+import Foundation
---
+++ Modulite/Screens/MajorChanges/MajorChangesLocalizedTexts.swift
@@ -0,0 +1,8 @@
+//
+//  MajorChangesLocalizedTexts.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 21/11/24.
+//
+
+import Foundation
