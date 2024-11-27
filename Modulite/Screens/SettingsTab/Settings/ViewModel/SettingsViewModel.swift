b29f2a56fc5dc9b47a2f700ed00976dfd72c5f7f
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-11-27 19:02:33 -0300

    Created new topics for terms of use and privacy policy

--- Modulite/Screens/SettingsTab/Settings/ViewModel/SettingsViewModel.swift
+++ Modulite/Screens/SettingsTab/Settings/ViewModel/SettingsViewModel.swift
@@ -13,6 +13,15 @@ enum Setting {
     case tutorials
     case faq
     case help
+    case termsOfUse
+    case privacyPolicy
+}
+
+enum SettingUrl: String {
+    // swiftlint:disable:next line_length
+    case termsOfUse = "https://perfect-daisy-6f1.notion.site/Modu-lite-s-Terms-of-Use-11abd895db1c8041b097eb2f6aeadb54?pvs=74"
+    // swiftlint:disable:next line_length
+    case privacyPolicy = "https://perfect-daisy-6f1.notion.site/Modu-lite-s-Privacy-Policy-11abd895db1c80d78fd6ddffabbf1e0b?pvs=4"
 }
 
 struct SettingCellData {
@@ -56,6 +65,24 @@ struct SettingCellData {
             titleKey: .settingsViewHelpTitle
         )
     }
+    
+    static func termsOfUse() -> Self {
+        .init(
+            setting: .termsOfUse,
+            symbolName: "text.document",
+            symbolColor: .burntEnds,
+            titleKey: .settingsViewTermsOfUseTitle
+        )
+    }
+    
+    static func privacyPolicy() -> Self {
+        .init(
+            setting: .privacyPolicy,
+            symbolName: "lock.document",
+            symbolColor: .burntCoconut,
+            titleKey: .settingsViewPrivacyPolicyTitle
+        )
+    }
 }
 
 class SettingsViewModel {
@@ -64,7 +91,9 @@ class SettingsViewModel {
         .subscription(),
         .tutorials(),
         .faq(),
-        .help()
+        .help(),
+        .termsOfUse(),
+        .privacyPolicy()
     ]
     
     // MARK: - Getters
@@ -79,5 +108,21 @@ class SettingsViewModel {
         }
         
         return settingsData[index]
+    }
+    
+    func getTermsOfUseURL() -> URL {
+        guard let url = URL(
+            string: SettingUrl.termsOfUse.rawValue
+        ) else { fatalError("Unable to create URL") }
+        
+        return url
+    }
+    
+    func getPrivacyPolicyURL() -> URL {
+        guard let url = URL(
+            string: SettingUrl.privacyPolicy.rawValue
+        ) else { fatalError("Unable to create URL") }
+        
+        return url
     }
 }
