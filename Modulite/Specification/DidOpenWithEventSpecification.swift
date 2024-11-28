ce02fba813e4052f662f020ef22b0aad06958305
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-11-28 20:10:50 -0300

    Added new specifications to check if should present offer plus

---
+++ Modulite/Specification/DidOpenWithEventSpecification.swift
@@ -0,0 +1,8 @@
+//
+//  DidOpenWithEventSpecification.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 28/11/24.
+//
+
+import Foundation
---
+++ Modulite/Specification/HasCompletedOnboardingSpecification.swift
@@ -0,0 +1,8 @@
+//
+//  HasCompletedOnboardingSpecification.swift
+//  Modulite
+//
+//  Created by Gustavo Munhoz Correa on 28/11/24.
+//
+
+import Foundation
